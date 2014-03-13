e264ab3941a80db26cd363f9ff28211b4e2572df
Danny van Kooten <dannyvankooten@gmail.com>
2014-03-13 21:17:00 +0100

    documented API class

--- includes/class-api.php
+++ includes/class-api.php
@@ -6,6 +6,10 @@ if( ! defined("MC4WP_LITE_VERSION") ) {
 	exit;
 }
 
+/**
+* Takes care of requests to the MailChimp API
+* @uses WP_HTTP
+*/ 
 class MC4WP_Lite_API {
 	
 	private $api_url = 'https://api.mailchimp.com/2.0/';
@@ -13,26 +17,49 @@ class MC4WP_Lite_API {
 	private $error_message = '';
 	private $connected = null;
 
-	public function __construct($api_key)
+	/**
+	* @param string $api_key MailChimp API key
+	*/
+	public function __construct( $api_key )
 	{
 		$this->api_key = $api_key;
 
-		if(strpos($api_key, '-') !== false) {
-			$this->api_url = 'https://' . substr($api_key, -3) . '.api.mailchimp.com/2.0/';
+		if( strpos( $api_key, '-' ) !== false ) {
+			$this->api_url = 'https://' . substr( $api_key, -3 ) . '.api.mailchimp.com/2.0/';
 		}
 	}
 
+	/**
+	* Pings the MailChimp API
+	* Will store its result to ensure a maximum of 1 ping per page load
+	*
+	* @return boolean
+	*/
 	public function is_connected()
 	{
-		if($this->connected === null) {
-			$result = $this->call('helper/ping');
-			$this->connected = ($result && isset($result->msg) && $result->msg === "Everything's Chimpy!");
+		if( $this->connected == null ) {
+			$result = $this->call( 'helper/ping' );
+			$this->connected = ( $result && isset( $result->msg ) && $result->msg === "Everything's Chimpy!" );
 		}
 		
 		return $this->connected;
 	}
 
-	public function subscribe($list_id, $email, array $merge_vars = array(), $email_type = 'html', $double_optin = true, $update_existing = false, $replace_interests = true, $send_welcome = false )
+	/**
+	* Sends a subscription request to the MailChimp API
+	*
+	* @param string $list_id
+	* @param string $email
+	* @param array $merge_vars
+	* @param string $email_type
+	* @param boolean $double_optin
+	* @param boolean $update_existing
+	* @param boolean $replace_interests
+	* @param boolean $send_welcome
+	*
+	* @return boolean|string True if success, 'error' if error
+	*/
+	public function subscribe( $list_id, $email, array $merge_vars = array(), $email_type = 'html', $double_optin = true, $update_existing = false, $replace_interests = true, $send_welcome = false )
 	{	
 		$data = array(
 			'id' => $list_id,
@@ -45,15 +72,17 @@ class MC4WP_Lite_API {
 			'send_welcome' => $send_welcome
 		);
 
-		$result = $this->call('lists/subscribe', $data);
+		$result = $this->call( 'lists/subscribe', $data );
 		
-		if($result) {
+		if( $result ) {
 
-			if(!isset($result->error)) {
+			if( ! isset( $result->error ) ) {
 				return true;
 			} else {
 				// check error
-				if($result->code == 214) {  return 'already_subscribed'; } 
+				if( $result->code == 214 ) {  
+					return 'already_subscribed'; 
+				} 
 				
 				// store error message
 				$this->error_message = $result->error;
@@ -65,62 +94,107 @@ class MC4WP_Lite_API {
 		}
 	}
 
-	public function get_list_groupings($list_id)
+	/**
+	* Gets the Groupings for a given List
+	* @param string $list_id
+	* @return array|boolean 
+	*/
+	public function get_list_groupings( $list_id )
 	{
-		$result = $this->call('lists/interest-groupings', array('id' => $list_id) );
-		if($result && is_array($result)) {
+		$result = $this->call( 'lists/interest-groupings', array( 'id' => $list_id ) );
+		if( $result && is_array( $result ) ) {
 			return $result;
 		} else {
 			return false;
 		}
 	}
 
+	/**
+	* Gets the lists for the current API Key
+	* @return array|boolean
+	*/
 	public function get_lists()
 	{
-		$result = $this->call('lists/list', array('limit' => 100));
+		$result = $this->call( 
+			'lists/list', 
+			array(
+				'limit' => 100
+			)
+		);
 
-		if($result && isset($result->data)) {
+		if( $result && isset( $result->data ) ) {
 			return $result->data;
 		} else {
 			return false;
 		}
 	}
 
-	public function get_lists_with_merge_vars($list_ids) 
+	/**
+	* Get lists with their merge_vars for a given array of list id's
+	* @param array $list_ids
+	* @return array|boolean
+	*/
+	public function get_lists_with_merge_vars( $list_ids ) 
 	{
-		$result = $this->call('lists/merge-vars', array('id' => $list_ids));
+		$result = $this->call( 'lists/merge-vars', array('id' => $list_ids ) );
 		
-		if($result && isset($result->data)) {
+		if( $result && isset( $result->data ) ) {
 			return $result->data;
 		} else {
 			return false;
 		}
 	}
 
-	public function get_member_info($list_id, $emails) {
-		$result = $this->call('lists/member-info', array('id' => $list_id, 'emails'  => $emails));
+	/**
+	* Gets the member info for one or multiple emails on a list
+	* 
+	* @param string $list_id
+	* @param array $emails
+	* @return array
+	*/
+	public function get_member_info( $list_id, $emails ) {
+		$result = $this->call( 'lists/member-info', array( 'id' => $list_id, 'emails'  => $emails ) );
 		
-		if($result && isset($result->data)) {
+		if( $result && isset( $result->data ) ) {
 			return $result->data;
 		} else {
 			return false;
 		}
 	}
 
-	public function list_has_subscriber($list_id, $email) {
-		$member_info = $this->get_member_info($list_id, array( array('email' => $email) ) );
+	/**
+	* Checks if an email address is on a given list
+	*
+	* @param string $list_id
+	* @param string $email
+	* @return boolean
+	*/
+	public function list_has_subscriber( $list_id, $email ) {
+		$member_info = $this->get_member_info( $list_id, array( array( 'email' => $email ) ) );
 
-		if( $member_info && is_array($member_info) ) {
-			return ($member_info[0]->status == "subscribed");
+		if( $member_info && is_array( $member_info ) ) {
+			return ( $member_info[0]->status == "subscribed" );
 		}
 
 		return false;
 	}
 
-	public function call($method, array $data = array())
+	/**
+	* Calls the MailChimp API
+	*
+	* @uses WP_HTTP
+	*
+	* @param string $method
+	* @param array $data
+	*
+	* @return object
+	*/
+	public function call( $method, array $data = array() )
 	{	
 		// do not make request when no api key was provided.
-		if(empty($this->api_key)) { return false; }
+		if( empty( $this->api_key ) ) { 
+			return false; 
+		}
 
 		$data['apikey'] = $this->api_key;
 		$url = $this->api_url . $method . '.json';
@@ -133,24 +207,32 @@ class MC4WP_Lite_API {
 			) 
 		); 
 	
-		if(is_wp_error($response)) {
+		if( is_wp_error( $response ) ) {
 			return false;
-		} else {
-			// dirty fix for older WP version
-			if($method == 'helper/ping' && isset($response['headers']['content-length']) && (int) $response['headers']['content-length'] == '44') { 
-				return (object) array( 'msg' => "Everything's Chimpy!");
-			}
-			
-			$body = wp_remote_retrieve_body($response);
-			return json_decode($body);
 		}
+
+		// dirty fix for older WP version
+		if($method == 'helper/ping' && isset( $response['headers']['content-length'] ) && (int) $response['headers']['content-length'] == '44' ) { 
+			return (object) array( 'msg' => "Everything's Chimpy!");
+		}
+		
+		$body = wp_remote_retrieve_body( $response );
+		return json_decode( $body );
 	}
 
+	/**
+	* Checks if an error occured in the most recent request
+	* @return boolean
+	*/
 	public function has_error()
 	{
-		return (!empty($this->error_message));
+		return ( ! empty( $this->error_message ) );
 	}
 
+	/**
+	* Gets the most recent error message
+	* @return string
+	*/
 	public function get_error_message()
 	{
 		return $this->error_message;
