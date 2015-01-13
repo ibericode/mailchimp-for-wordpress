5f108e0565ee07e6518851dc6b24842f92606d7a
Danny van Kooten <dannyvankooten@gmail.com>
2015-01-13 23:15:43 +0700

    Account for MailChimp server names with more than 3 characters. Fixes #65 .

--- includes/class-api.php
+++ includes/class-api.php
@@ -41,9 +41,10 @@ class MC4WP_Lite_API {
 	public function __construct( $api_key )
 	{
 		$this->api_key = $api_key;
+		$dash_position = strpos( $api_key, '-' );
 
-		if( strpos( $api_key, '-' ) !== false ) {
-			$this->api_url = 'https://' . substr( $api_key, -3 ) . '.api.mailchimp.com/2.0/';
+		if( $dash_position !== false ) {
+			$this->api_url = 'https://' . substr( $api_key, $dash_position + 1 ) . '.api.mailchimp.com/2.0/';
 		}
 	}
 
