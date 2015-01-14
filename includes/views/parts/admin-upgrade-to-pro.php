94e862fbe55e6aa0793917d04e115c42568f6809
Danny van Kooten <dannyvankooten@gmail.com>
2015-01-14 13:17:41 +0700

    Change button classes for CTA

--- includes/views/parts/admin-upgrade-to-pro.php
+++ includes/views/parts/admin-upgrade-to-pro.php
@@ -10,5 +10,5 @@ if( ! defined("MC4WP_LITE_VERSION") ) {
 	
 	<p><em><?php _e( 'This plugin has an even better premium version, I am sure you will love it.', 'mailchimp-for-wp' ); ?></em></p>
 	<p><?php _e( 'Pro features include better and multiple forms, advanced and easy form styling, more default themes, detailed statistics and priority support.', 'mailchimp-for-wp' ); ?></p>
-	<p><a class="button-primary" href="https://mc4wp.com/#utm_source=lite-plugin&utm_medium=link&utm_campaign=upgrade-box"><?php _e( 'Upgrade Now', 'mailchimp-for-wp' ); ?> &raquo;</a></p>
+	<p><a class="button button-primary button-large" href="https://mc4wp.com/#utm_source=lite-plugin&utm_medium=link&utm_campaign=upgrade-box"><?php _e( 'Upgrade Now', 'mailchimp-for-wp' ); ?> &raquo;</a></p>
 </div>
