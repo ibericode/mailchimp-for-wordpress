a20e748b862395ca6a4808d29a0e294238457bbb
Danny van Kooten <dannyvankooten@gmail.com>
2015-02-03 13:15:53 +0700

    Add tests covering error setters and grouping & merge var API responses.

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -101,4 +101,74 @@ class ApiTest extends PHPUnit_Framework_TestCase {
 		$this->api->set_response( (object) array( 'data' => $lists ) );
 		$this->assertEquals( $this->api->get_lists(), $lists );
 	}
+
+	/**
+	 * @covers MC4WP_API::get_list_groupings
+	 */
+	public function test_get_list_groupings() {
+		// test error
+		$this->api->set_response( false );
+		$this->assertFalse( $this->api->get_list_groupings( 'list_id') );
+
+		// test api error
+		$this->api->set_response( (object) array( 'error' => 'Error message', 'code' => -99 ) );
+		$this->assertFalse( $this->api->get_list_groupings( 'list_id' ) );
+
+		// test success
+		$groups = array( (object) array( 'id' => 1, 'name' => 'Group Name' ) );
+		$this->api->set_response( $groups );
+		$this->assertEquals( $this->api->get_list_groupings( 'list_id' ), $groups );
+	}
+
+	/**
+	 * @covers MC4WP_API::get_lists_with_merge_vars
+	 */
+	public function test_get_lists_with_merge_vars() {
+		// test error
+		$this->api->set_response( false );
+		$this->assertFalse( $this->api->get_lists_with_merge_vars( array( 1, 2 ) ) );
+
+		// test api error
+		$this->api->set_response( (object) array( 'error' => 'Error message', 'code' => -99 ) );
+		$this->assertFalse( $this->api->get_lists_with_merge_vars( array( 1, 2 ) ) );
+
+		// test success
+		$lists = array( 'sample_list' );
+		$this->api->set_response( (object) array( 'data' => $lists ) );
+		$this->assertEquals( $this->api->get_lists_with_merge_vars(  array( 1, 2 ) ), $lists );
+	}
+
+	/**
+	 * @covers MC4WP_API::has_error
+	 */
+	public function test_has_error() {
+		// no error by default
+		$this->assertFalse( $this->api->has_error() );
+
+		// error should be stored after failed API request
+		$this->api->set_response( (object) array( 'error' => 'error message', 'code' => '-99' ) );
+		$this->api->subscribe( 'sample_list_id', 'sample_email' );
+		$this->assertTrue( $this->api->has_error() );
+	}
+
+	/**
+	 * @covers MC4WP_API::get_error
+	 */
+	public function test_get_error_message() {
+		// no error by default
+		$this->assertEmpty( $this->api->get_error_message() );
+
+		// error should be stored after failed API request
+		$this->api->set_response( (object) array( 'error' => 'error message', 'code' => '-99' ) );
+		$this->api->subscribe( 'sample_list_id', 'sample_email' );
+		$this->assertEquals( $this->api->get_error_message(), 'error message' );
+	}
+
+	/**
+	 * @covers MC4WP_API::get_last_response
+	 */
+	public function test_get_last_response() {
+		// no response by default
+		$this->assertNull( $this->api->get_last_response() );
+	}
 }
