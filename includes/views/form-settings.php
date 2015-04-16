3be23530a81e1bd690871f408e54680a3dcee2c0
Danny van Kooten <dannyvankooten@gmail.com>
2015-04-16 17:23:25 +0200

    Allow entering `0` for no redirect after form success.

--- includes/views/form-settings.php
+++ includes/views/form-settings.php
@@ -188,7 +188,7 @@ if( ! defined( 'MC4WP_LITE_VERSION' ) ) {
 				<th scope="row"><label for="mc4wp_form_redirect"><?php _e( 'Redirect to URL after successful sign-ups', 'mailchimp-for-wp' ); ?></label></th>
 				<td colspan="2">
 					<input type="text" class="widefat" name="mc4wp_lite_form[redirect]" id="mc4wp_form_redirect" placeholder="<?php printf( __( 'Example: %s', 'mailchimp-for-wp' ), esc_url( site_url( '/thank-you/' ) ) ); ?>" value="<?php echo esc_url( $opts['redirect'] ); ?>" />
-					<p class="help"><?php _e( 'Leave empty for no redirect. Otherwise, use complete (absolute) URLs, including <code>http://</code>.', 'mailchimp-for-wp' ); ?></p>
+					<p class="help"><?php _e( 'Leave empty or enter <code>0</code> for no redirect. Otherwise, use complete (absolute) URLs, including <code>http://</code>.', 'mailchimp-for-wp' ); ?></p>
 				</td>
 			</tr>
 			<tr valign="top">
