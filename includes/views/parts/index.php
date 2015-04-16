9dbe6e0ba24fa79a34093c83d4573ca395d733b5
Danny van Kooten <dannyvankooten@gmail.com>
2015-04-16 17:22:32 +0200

    Remove index.php files. Were used to prevent directory listing but should be handled by server.

--- includes/views/parts/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-if( ! defined( 'MC4WP_LITE_VERSION' ) ) {
-	header( 'Status: 403 Forbidden' );
-	header( 'HTTP/1.1 403 Forbidden' );
-	exit;
-}
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// prevent directory listing
-
-header( 'Status: 403 Forbidden' );
-header( 'HTTP/1.1 403 Forbidden' );
-exit;
