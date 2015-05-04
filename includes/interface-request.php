bb1344b2a16f20b7f7f8473d58e3f152f1f406c4
Danny van Kooten <dannyvankooten@gmail.com>
2015-05-04 16:21:57 +0200

    Add documentation to interface class

--- includes/interface-request.php
+++ includes/interface-request.php
@@ -1,8 +1,32 @@
 <?php
 
 interface iMC4WP_Request {
+
+	/**
+	 * Prepare the given request data so it matches the format expected by the process() method.
+	 *
+	 * @return bool
+	 */
 	public function prepare();
+
+	/**
+	 * Validate the request
+	 *
+	 * @return bool
+	 */
 	public function validate();
+
+	/**
+	 * Process the request any way you like
+	 *
+	 * @return bool
+	 */
 	public function process();
+
+	/**
+	 * Respond to the request any way you see fit
+	 *
+	 * @return bool
+	 */
 	public function respond();
 }
