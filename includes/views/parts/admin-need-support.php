1555a0d419243400493b2bc493b7b87cbaf150de
Danny van Kooten <dannyvankooten@gmail.com>
2015-05-05 21:23:42 +0200

    Link to affiliate onboarding page to promote people writing blog posts about the plugin.

--- includes/views/parts/admin-need-support.php
+++ includes/views/parts/admin-need-support.php
@@ -16,7 +16,7 @@ if( ! defined( 'MC4WP_LITE_VERSION' ) ) {
 		<li><a target="_blank" href="https://mc4wp.com/"><?php printf( __( 'Upgrade to MailChimp for WordPress Pro', 'mailchimp-for-wp' ), 'https://mc4wp.com/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&show-appreciation' ); ?></a></li>
 		<li><a target="_blank" href="https://wordpress.org/support/view/plugin-reviews/mailchimp-for-wp?rate=5#postform"><?php printf( __( 'Leave a %s plugin review on WordPress.org', 'mailchimp-for-wp' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ); ?></a></li>
 		<li><a target="_blank" href="https://twitter.com/?status=<?php echo urlencode( __( 'I am using MailChimp for WordPress by @DannyvanKooten - it is great!', 'mailchimp-for-wp' ) . ' > https://mc4wp.com/' ); ?>"><?php _e( 'Tweet about MailChimp for WordPress', 'mailchimp-for-wp' ); ?></a></li>
-		<li><?php echo sprintf( __( 'Review the plugin on your blog and link to <a href="%s">the plugin page</a>.', 'mailchimp-for-wp' ), 'https://mc4wp.com/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&utm_campaign=show-appreciation' ) . ' ' . __( 'There is an <a href="%s">affiliate program</a> as well.', 'mailchimp-for-wp' ); ?></li>
+		<li><?php echo sprintf( __( 'Review the plugin on your blog and link to <a href="%s">the plugin page</a>.', 'mailchimp-for-wp' ), 'https://mc4wp.com/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&utm_campaign=show-appreciation' ) . ' ' . sprintf( __( 'There is an <a href="%s">affiliate program</a> as well.', 'mailchimp-for-wp' ), 'https://mc4wp.com/affiliates/' ); ?></li>
 		<li><a target="_blank" href="https://wordpress.org/plugins/mailchimp-for-wp/"><?php _e( 'Vote "works" on the WordPress.org plugin page', 'mailchimp-for-wp' ); ?></a></li>
 	</ul>
 </div>
