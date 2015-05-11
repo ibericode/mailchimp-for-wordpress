0618786daeb98b392bac82012bc97d0e352fd28f
Danny van Kooten <dannyvankooten@gmail.com>
2015-05-11 17:27:02 +0200

    Get correct IP address. Fixes #99 .

--- includes/class-tools.php
+++ includes/class-tools.php
@@ -130,7 +130,18 @@ class MC4WP_Tools {
 	 * @return string
 	 */
 	public static function get_client_ip() {
-		return strip_tags( $_SERVER['REMOTE_ADDR'] );
+
+		$headers = ( function_exists( 'apache_request_headers' ) ) ? apache_request_headers() : $_SERVER;
+
+		if ( array_key_exists( 'X-Forwarded-For', $headers ) && filter_var( $headers['X-Forwarded-For'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+			$ip = $headers['X-Forwarded-For'];
+		} elseif ( array_key_exists( 'HTTP_X_FORWARDED_FOR', $headers ) && filter_var( $headers['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ) {
+			$ip = $headers['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$ip = filter_var( $_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 );
+		}
+
+		return $ip;
 	}
 
 	/**
