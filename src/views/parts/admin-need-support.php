4562ccdccd35e3d9c7aab3e055b9fcecf29668c2
Danny van Kooten <dannyvankooten@gmail.com>
2015-06-23 10:56:36 +0200

    Update Twitter mention, closes #124

--- src/views/parts/admin-need-support.php
+++ src/views/parts/admin-need-support.php
@@ -1,9 +1,6 @@
 <?php
-if( ! defined( 'MC4WP_VERSION' ) ) {
-	header( 'Status: 403 Forbidden' );
-	header( 'HTTP/1.1 403 Forbidden' );
-	exit;
-}
+// die if accessed directly
+defined( 'ABSPATH' ) or exit;
 ?>
 <div class="mc4wp-box">
 	<h4 class="mc4wp-title"><?php _e( 'Looking for help?', 'mailchimp-for-wp' ); ?></h4>
@@ -15,7 +12,7 @@ if( ! defined( 'MC4WP_VERSION' ) ) {
 	<ul class="ul-square">
 		<li><a target="_blank" href="https://mc4wp.com/"><?php printf( __( 'Upgrade to MailChimp for WordPress Pro', 'mailchimp-for-wp' ), 'https://mc4wp.com/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&show-appreciation' ); ?></a></li>
 		<li><a target="_blank" href="https://wordpress.org/support/view/plugin-reviews/mailchimp-for-wp?rate=5#postform"><?php printf( __( 'Leave a %s plugin review on WordPress.org', 'mailchimp-for-wp' ), '&#9733;&#9733;&#9733;&#9733;&#9733;' ); ?></a></li>
-		<li><a target="_blank" href="https://twitter.com/?status=<?php echo urlencode( __( 'I am using MailChimp for WordPress by @DannyvanKooten - it is great!', 'mailchimp-for-wp' ) . ' > https://mc4wp.com/' ); ?>"><?php _e( 'Tweet about MailChimp for WordPress', 'mailchimp-for-wp' ); ?></a></li>
+		<li><a target="_blank" href="https://twitter.com/?status=<?php echo urlencode( __( 'I am using MailChimp for WordPress by @ibericode - it is great!', 'mailchimp-for-wp' ) . ' > https://mc4wp.com/' ); ?>"><?php _e( 'Tweet about MailChimp for WordPress', 'mailchimp-for-wp' ); ?></a></li>
 		<li><?php echo sprintf( __( 'Review the plugin on your blog and link to <a href="%s">the plugin page</a>.', 'mailchimp-for-wp' ), 'https://mc4wp.com/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&utm_campaign=show-appreciation' ) . ' ' . sprintf( __( 'There is an <a href="%s">affiliate program</a> as well.', 'mailchimp-for-wp' ), 'https://mc4wp.com/affiliates/' ); ?></li>
 		<li><a target="_blank" href="https://wordpress.org/plugins/mailchimp-for-wp/"><?php _e( 'Vote "works" on the WordPress.org plugin page', 'mailchimp-for-wp' ); ?></a></li>
 	</ul>
