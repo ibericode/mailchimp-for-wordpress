4b6781dcae37bc078d40ca8960db8aaa5c5a7e1c
Danny van Kooten <dannyvankooten@gmail.com>
2015-07-01 13:58:58 +0200

    Update form assets with new form reset

--- src/Forms/Assets.php
+++ src/Forms/Assets.php
@@ -89,7 +89,7 @@ class MC4WP_Forms_Assets {
 			default:
 			case 'default':
 			case 1:
-				wp_enqueue_style( 'mailchimp-for-wp-form', MC4WP_PLUGIN_URL . 'assets/css/form-reset' . $suffix . '.css', array(), MC4WP_VERSION, 'all' );
+				wp_enqueue_style( 'mailchimp-for-wp-form', MC4WP_PLUGIN_URL . 'assets/css/form-basic' . $suffix . '.css', array(), MC4WP_VERSION, 'all' );
 				break;
 
 		}
