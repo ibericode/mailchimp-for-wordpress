58ed8838905ded23add985399c71a995c1dd7dd5
Danny van Kooten <dannyvankooten@gmail.com>
2015-08-16 12:54:01 +0700

    Add email opt-in to sidebar on admin pages.

--- includes/views/parts/admin-upgrade-to-pro.php
+++ includes/views/parts/admin-upgrade-to-pro.php
@@ -32,3 +32,28 @@ if( ! defined( 'MC4WP_LITE_VERSION' ) ) {
 		<a class="button" href="https://mc4wp.com/demo/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&utm_campaign=upgrade-box"><?php _e( 'View Demo', 'mailchimp-for-wp' ); ?></a>
 	</p>
 </div>
+<div class="mc4wp-box" id="mc4wp-optin-box">
+
+	<?php $user = wp_get_current_user(); ?>
+	<!-- Begin MailChimp Signup Form -->
+	<div id="mc_embed_signup">
+		<h4 class="mc4wp-title"><?php _e( 'More subscribers, better newsletters.', 'mailchimp-for-wp' ); ?></h4>
+		<p><?php _e( 'Learn how to best grow your lists & write better emails by subscribing to our monthly tips.', 'mailchimp-for-wp' ); ?></p>
+		<form action="//mc4wp.us1.list-manage.com/subscribe/post?u=a2d08947dcd3683512ce174c5&amp;id=a940232df9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+			<p>
+				<label>Email Address </label>
+				<input type="email" value="<?php echo esc_attr( $user->user_email ); ?>" name="EMAIL" class="regular-text" required>
+			</p>
+			<p>
+				<label>First Name </label>
+				<input type="text" value="<?php echo esc_attr( $user->user_firstname ); ?>" name="FNAME" class="regular-text" id="mce-FNAME">
+			</p>
+			<div style="position: absolute; left: -5000px;">
+				<input type="text" name="b_a2d08947dcd3683512ce174c5_a940232df9" tabindex="-1" value="" />
+			</div>
+			<p>
+				<input type="submit" value="Subscribe" name="subscribe" class="button">
+			</p>
+		</form>
+	</div>
+</div>
