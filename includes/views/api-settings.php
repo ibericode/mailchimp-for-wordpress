d11666842c151f6f25dd7548ed6d7048a203f45f
Danny van Kooten <dannyvankooten@gmail.com>
2015-08-16 14:28:12 +0700

    Revamped UI for Lists overview table on MailChimp settings page. Much more mobile friendly + showing field type. Closes #139

--- includes/views/api-settings.php
+++ includes/views/api-settings.php
@@ -55,68 +55,86 @@ if( ! defined( 'MC4WP_LITE_VERSION' ) ) {
 			</p>
 		</form>
 
-		<table class="wp-list-table widefat">
-			<thead>
-				<tr>
-					<th class="mc4wp-hide-smallscreens" scope="col">List ID</th>
-					<th scope="col">List Name</th>
-					<th scope="col">Merge Fields <code>TAG</code></th>
-					<th scope="col">Groupings</th>
-					<th class="mc4wp-hide-smallscreens" scope="col">Subscribers</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				if( ! empty( $lists ) && is_array( $lists ) ) {
-					foreach($lists as $list) { ?>
+		<div class="mc4wp-lists-overview" style="background: white; padding: 20px; max-height: 500px; overflow-y: scroll;">
+		<?php if( empty( $lists ) || ! is_array( $lists ) ) { ?>
+			<p><?php _e( 'No lists were found in your MailChimp account', 'mailchimp-for-wp' ); ?>.</p>
+		<?php } else {
 
-					<tr valign="top">
-						<td class="mc4wp-hide-smallscreens"><?php echo esc_html( $list->id ); ?></td>
-						<td><?php echo esc_html( $list->name ); ?></td>
-						
-						<td>
-							<?php if( ! empty( $list->merge_vars ) && is_array( $list->merge_vars ) ) { ?>
-								<ul class="ul-square" style="margin-top: 0;">
-									<?php foreach( $list->merge_vars as $merge_var ) { ?>
-										<li><?php echo esc_html( $merge_var->name ); if( $merge_var->req ) { echo '<span style="color:red;">*</span>'; } ?> <code><?php echo esc_html( $merge_var->tag ); ?></code></li>
+			printf( '<p>' . __( 'A total of %d lists were found in your MailChimp account.', 'mailchimp-for-wp' ) . '</p>', count( $lists ) );
+
+			foreach ( $lists as $list ) { ?>
+
+				<table class="widefat" cellspacing="0">
+					<tr>
+						<td colspan="2"><h3><?php echo esc_html( $list->name ); ?></h3></td>
+					</tr>
+					<tr>
+						<th width="150">List ID</th>
+						<td><?php echo esc_html( $list->id ); ?></td>
+					</tr>
+					<tr>
+						<th># of subscribers</th>
+						<td><?php echo esc_html( $list->subscriber_count ); ?></td>
+					</tr>
+					<tr>
+						<th>Fields</th>
+						<td style="padding: 0; ">
+							<?php if ( ! empty( $list->merge_vars ) && is_array( $list->merge_vars ) ) { ?>
+								<table class="widefat fixed" cellspacing="0">
+									<thead>
+									<tr>
+										<th>Name</th>
+										<th>Tag</th>
+										<th>Type</th>
+									</tr>
+									</thead>
+									<?php foreach ( $list->merge_vars as $merge_var ) { ?>
+										<tr title="<?php printf( __( '%s (%s) with field type %s.', 'mailchimp-for-wp' ), esc_html( $merge_var->name ), esc_html( $merge_var->tag ), esc_html( $merge_var->field_type ) ); ?>">
+											<td><?php echo esc_html( $merge_var->name );
+												if ( $merge_var->req ) {
+													echo '<span style="color:red;">*</span>';
+												} ?></td>
+											<td><code><?php echo esc_html( $merge_var->tag ); ?></code></td>
+											<td><?php echo esc_html( $merge_var->field_type ); ?></td>
+										</tr>
 									<?php } ?>
-								</ul>
+								</table>
 							<?php } ?>
 						</td>
-						<td>
-						<?php
-						if( ! empty( $list->interest_groupings ) && is_array( $list->interest_groupings ) ) {
-							foreach($list->interest_groupings as $grouping) { ?>
-								<strong><?php echo esc_html( $grouping->name ); ?></strong>
-
-								<?php if( ! empty( $grouping->groups ) && is_array( $grouping->groups ) ) { ?>
-									<ul class="ul-square">
-										<?php foreach( $grouping->groups as $group ) { ?>
-											<li><?php echo esc_html( $group->name ); ?></li>
-										<?php } ?>
-									</ul>
-								<?php } ?>
-							<?php }
-						} else {
-							?>-<?php
-						} ?>
-
-						</td>
-						<td class="mc4wp-hide-smallscreens"><?php echo esc_html( $list->subscriber_count ); ?></td>
 					</tr>
-					<?php
-					}
-				} else { ?>
-					<tr>
-						<td colspan="5">
-							<p><?php _e( 'No lists were found in your MailChimp account', 'mailchimp-for-wp' ); ?>.</p>
-						</td>
-					</tr>
-				<?php
-				}
-				?>
-			</tbody>
-		</table>
+					<?php if ( ! empty( $list->interest_groupings ) && is_array( $list->interest_groupings ) ) { ?>
+						<tr>
+							<th>Interest Groupings</th>
+							<td style="padding: 0;">
+								<table class="widefat fixed" cellspacing="0">
+									<thead>
+									<tr>
+										<th>Name</th>
+										<th>Groups</th>
+									</tr>
+									</thead>
+									<?php foreach ( $list->interest_groupings as $grouping ) { ?>
+										<tr title="<?php esc_attr( printf( __( '%s (ID: %s) with type %s.', 'mailchimp-for-wp' ), $grouping->name, $grouping->id, $grouping->form_field ) ); ?>">
+											<td><?php echo esc_html( $grouping->name ); ?></td>
+											<td>
+												<ul class="ul-square">
+													<?php foreach ( $grouping->groups as $group ) { ?>
+														<li><?php echo esc_html( $group->name ); ?></li>
+													<?php } ?>
+												</ul>
+											</td>
+										</tr>
+									<?php } ?>
+								</table>
+
+							</td>
+						</tr>
+					<?php } ?>
+				</table>
+				<br style="margin: 20px 0;" />
+			<?php } // end foreach $lists
+		} // end if empty ?>
+		</div>
 
 		<form method="post" action="">
 			<input type="hidden" name="mc4wp-renew-cache" value="1" />
