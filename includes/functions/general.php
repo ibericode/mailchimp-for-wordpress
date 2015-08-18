0beeaea9fd2aa96481a5e385ccac6ca0fd4e1c2c
Danny van Kooten <dannyvankooten@gmail.com>
2015-08-18 11:33:38 +0700

    Add `mc4wp_form_is_submitted` and `mc4wp_form_get_response_html` methods. Closes #131

--- includes/functions/general.php
+++ includes/functions/general.php
@@ -87,9 +87,32 @@ function mc4wp_get_options( $key = '' ) {
 
 /**
 * Gets the MailChimp for WP API class and injects it with the given API key
-*
+* @since 1.0
 * @return MC4WP_API
 */
 function mc4wp_get_api() {
 	return MC4WP_Lite::instance()->get_api();
+}
+
+/**
+ * Check whether a form was submitted
+ *
+ * @since 2.3.8
+ * @param int $form_id The ID of the form you want to check. (optional)
+ * @param string $element_id The ID of the form element you want to check, eg id="mc4wp-form-1" (optional)
+ * @return boolean
+ */
+function mc4wp_form_is_submitted( $form_id = 0, $element_id = null ) {
+	$form = MC4WP_Form::get();
+	return $form->is_submitted( $element_id );
+}
+
+/**
+ * @since 2.3.8
+ * @param int $form_id
+ * @return string
+ */
+function mc4wp_form_get_response_html( $form_id = 0 ) {
+	$form = MC4WP_Form::get();
+	return $form->request->get_response_html();
 }
