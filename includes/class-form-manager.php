535f3a10b1fce88fc5aa6befb8aedfd68f0cde5d
Danny van Kooten <dannyvankooten@gmail.com>
2015-09-14 16:38:38 +0700

    Run `do_shortcode` on form content using a filter. Allows the use of shortcodes inside forms. Closes #165

--- includes/class-form-manager.php
+++ includes/class-form-manager.php
@@ -54,6 +54,9 @@ class MC4WP_Lite_Form_Manager {
 		// enable shortcodes in text widgets
 		add_filter( 'widget_text', 'shortcode_unautop' );
 		add_filter( 'widget_text', 'do_shortcode', 11 );
+
+		// enable shortcodes in form content
+		add_filter( 'mc4wp_form_content', 'do_shortcode' );
 	}
 
 	/**
