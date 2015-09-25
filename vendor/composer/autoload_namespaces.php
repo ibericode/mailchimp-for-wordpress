a265e9d4098ccbba0b80d00496c46bb5cd16c31f
Danny van Kooten <dannyvankooten@gmail.com>
2015-09-25 15:19:05 +0200

    updated autoloader

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,13 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'xrstf\\Composer52' => array($vendorDir . '/xrstf/composer-php52/lib'),
-    'Composer\\Installers\\' => array($vendorDir . '/composer/installers/src'),
+    'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
+    'Guzzle\\Tests' => array($vendorDir . '/guzzle/guzzle/tests'),
+    'Guzzle' => array($vendorDir . '/guzzle/guzzle/src'),
+    'Contrib\\Component' => array($vendorDir . '/satooshi/php-coveralls/src'),
+    'Contrib\\Bundle' => array($vendorDir . '/satooshi/php-coveralls/src'),
+    'CodeClimate\\Component' => array($vendorDir . '/codeclimate/php-test-reporter/src'),
+    'CodeClimate\\Bundle' => array($vendorDir . '/codeclimate/php-test-reporter/src'),
 );
