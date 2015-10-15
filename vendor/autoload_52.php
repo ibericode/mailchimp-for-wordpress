7af4110874c06d66b02a265aa7c1a07a6d0552e2
Danny van Kooten <dannyvankooten@gmail.com>
2015-10-15 15:33:47 +0700

    Update PHP 5.2 compatible autoloader so it stops throwing errors on Travis.

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit2f909f1fbc22fbc2d15e30f4553f3491::getLoader();
+return ComposerAutoloaderInitc98d91d1c0d574e4aafcd3ceced160db::getLoader();
