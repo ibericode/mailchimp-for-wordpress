c2f0f5372f981de9f17cb7c57ae9b92c22da76f5
Danny van Kooten <dannyvankooten@gmail.com>
2015-10-21 14:15:52 +0700

    Fix incorrect `wp_redirect` use.

--- includes/class-ads.php
+++ includes/class-ads.php
@@ -52,7 +52,7 @@ class MC4WP_Ads {
 		$url = 'https://mc4wp.com/#utm_source=wp-plugin&utm_medium=mailchimp-for-wp&utm_campaign=menu-upgrade-link';
 
 		if( ! headers_sent() ) {
-			wp_redirect( "Location: $url;", 302 );
+			wp_redirect( $url, 302 );
 			exit;
 		} else {
 			echo '<p>' . sprintf( __( 'You will be redirected to <strong>%s</strong> in a few seconds. <a href="%s">Click here if you are not automatically redirected.</a>', 'mailchimp-for-wp' ), 'mc4wp.com', $url ) . '</p>';
@@ -70,8 +70,8 @@ class MC4WP_Ads {
 			'title' => __( 'Upgrade to Pro', 'mailchimp-for-wp' ),
 			'text' => '<span style="line-height: 20px;"><span class="dashicons dashicons-external"></span> ' .__( 'Upgrade to Pro', 'mailchimp-for-wp' ),
 			'slug' => 'upgrade',
-
 			'callback' => array( $this, 'redirect_to_pro' ),
+			'load_callback' => array( $this, 'redirect_to_pro' ),
 		);
 
 		return $items;
