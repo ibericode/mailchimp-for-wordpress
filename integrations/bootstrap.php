1e90d517f519e948fbd228703e583c882befb3c3
Danny van Kooten <dannyvankooten@gmail.com>
2015-10-22 17:00:05 +0700

    Always enable CF7, Custom and Events Manager integration

--- integrations/bootstrap.php
+++ integrations/bootstrap.php
@@ -21,6 +21,6 @@ mc4wp_add_integration( 'wp-registration-form', 'MC4WP_Registration_Form_Integrat
 mc4wp_add_integration( 'buddypress', 'MC4WP_BuddyPress_Integration' );
 mc4wp_add_integration( 'woocommerce', 'MC4WP_WooCommerce_Integration' );
 mc4wp_add_integration( 'easy-digital-downloads', 'MC4WP_Easy_Digital_Downloads_Integration' );
-mc4wp_add_integration( 'contact-form-7', 'MC4WP_Contact_Form_7_Integration' );
-mc4wp_add_integration( 'events-manager', 'MC4WP_Events_Manager_Integration' );
-mc4wp_add_integration( 'custom', 'MC4WP_Custom_Integration' );
+mc4wp_add_integration( 'contact-form-7', 'MC4WP_Contact_Form_7_Integration', true );
+mc4wp_add_integration( 'events-manager', 'MC4WP_Events_Manager_Integration', true );
+mc4wp_add_integration( 'custom', 'MC4WP_Custom_Integration', true );
