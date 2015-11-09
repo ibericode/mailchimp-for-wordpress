6a945c735795fb261192d93423c5751caeaa9b00
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-09 15:27:31 +0700

    Show GitHub notice on hosts that start with `local`.

--- includes/views/parts/admin-footer.php
+++ includes/views/parts/admin-footer.php
@@ -6,7 +6,8 @@
 		<p class="help"><?php printf( __( 'MailChimp for WordPress is in need of translations. Is the plugin not translated in your language or do you spot errors with the current translations? Helping out is easy! Head over to <a href="%s">the translation project and click "help translate"</a>.', 'mailchimp-for-wp' ), 'https://www.transifex.com/projects/p/mailchimp-for-wordpress/' ); ?></p>
 	<?php } ?>
 
-	<?php if( defined( 'WP_DEBUG' ) && WP_DEBUG ) { ?>
+	<?php
+	if( strpos( $_SERVER['HTTP_HOST'], 'local' ) === 0 ) { ?>
 		<p class="help">Developer? Follow or contribute to the <a href="https://github.com/ibericode/mailchimp-for-wordpress">MailChimp for WordPress project on GitHub</a>.</p>
 	<?php } ?>
 
