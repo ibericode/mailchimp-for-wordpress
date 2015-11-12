84767aad647fd0760edac3a1976341fcb63ec32b
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-12 13:33:13 +0700

    Add `.code-sample` styling for `<textarea>` and `<input>` elements containing a piece of code.

--- integrations/custom/admin-before.php
+++ integrations/custom/admin-before.php
@@ -2,8 +2,10 @@
 	<?php _e( 'To get a custom integration to work, include the following HTML in the form you are trying to integrate with.', 'mailchimp-for-wp' ); ?>
 </p>
 
-<pre style="display: block; background: white; border: 1px solid #333; padding: 10px;"><?php ob_start(); ?><p>
+<textarea style="width: 100%;" class="code-sample" rows="5" readonly onfocus="this.select()"><?php ob_start(); ?><p>
 	<label>
 		<input type="checkbox" name="mc4wp-subscribe" value="1" />
+		Subscribe to our newsletter.
 	</label>
-</p><?php $html = ob_get_clean(); echo esc_html( $html ); ?></pre>
+</p><?php $html = ob_get_clean(); echo esc_textarea( $html ); ?>
+</textarea>
