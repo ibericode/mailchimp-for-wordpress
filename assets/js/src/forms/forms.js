e707f9840254ae9964f240d62d7282b071a58cfd
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-12 17:38:58 +0700

    Remove unneeded scope.

--- assets/js/src/forms/forms.js
+++ assets/js/src/forms/forms.js
@@ -1,68 +1,67 @@
-var forms = function() {
-	'use strict';
+'use strict';
 
-	// deps
-	var EventEmitter = require('../../third-party/event-emitter.js');
-	var Form = require('./form.js');
+// deps
+var EventEmitter = require('../../third-party/event-emitter.js');
+var Form = require('./form.js');
 
-	// variables
-	var events = new EventEmitter();
-	var forms = [];
+// variables
+var events = new EventEmitter();
+var forms = [];
 
-	// get form by its id
-	function get(formId) {
-		for(var i=0; i<forms.length;i++) {
-			if(forms[i].id == formId) return forms[i];
+// get form by its id
+function get(formId) {
+	for(var i=0; i<forms.length;i++) {
+		if(forms[i].id == formId) {
+			return forms[i];
 		}
-
-		var formElement = document.querySelector('.mc4wp-form-' + formId);
-		return createFromElement(formElement,formId);
 	}
 
-	// get form by <form> element (or any input in form)
-	function getByElement(element) {
-		var formElement = element.form || element;
-		for(var i=0; i<forms.length;i++) {
-			if(forms[i].element == formElement) return forms[i];
+	var formElement = document.querySelector('.mc4wp-form-' + formId);
+	return createFromElement(formElement,formId);
+}
+
+// get form by <form> element (or any input in form)
+function getByElement(element) {
+	var formElement = element.form || element;
+	for(var i=0; i<forms.length;i++) {
+		if(forms[i].element == formElement) {
+			return forms[i];
 		}
-
-		return createFromElement(element);
 	}
 
-	// create form object from <form> element
-	function createFromElement(formElement,id) {
-		id = id || parseInt( formElement.getAttribute('data-id') ) || 0;
-		var form = new Form(id,formElement);
-		forms.push(form);
-		return form;
-	}
+	return createFromElement(element);
+}
 
-	function all() {
-		return forms;
-	}
+// create form object from <form> element
+function createFromElement(formElement,id) {
+	id = id || parseInt( formElement.getAttribute('data-id') ) || 0;
+	var form = new Form(id,formElement);
+	forms.push(form);
+	return form;
+}
 
-	function on(event,callback) {
-		return events.on(event,callback);
-	}
+function all() {
+	return forms;
+}
 
-	function trigger(event,args) {
-		return events.trigger(event,args);
-	}
+function on(event,callback) {
+	return events.on(event,callback);
+}
 
-	function off(event,callback) {
-		return events.off(event,callback);
-	}
+function trigger(event,args) {
+	return events.trigger(event,args);
+}
 
-	// public API
-	return {
-		all: all,
-		get: get,
-		getByElement: getByElement,
-		on: on,
-		trigger: trigger,
-		off: off
-	}
+function off(event,callback) {
+	return events.off(event,callback);
+}
+
+module.exports = {
+	"all": all,
+	"get": get,
+	"getByElement": getByElement,
+	"on": on,
+	"trigger": trigger,
+	"off": off
 };
-
-module.exports = forms();
 
