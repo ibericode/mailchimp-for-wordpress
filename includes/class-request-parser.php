bad255194a66ac8a0890f5f606bf767dcce0e381
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-16 17:57:56 +0700

    Lowercase prefix.

--- includes/class-request-parser.php
+++ includes/class-request-parser.php
@@ -38,7 +38,8 @@ class MC4WP_Request_Parser {
 	 *
 	 * @return array
 	 */
-	public function namespaced( $namespace = 'MC4WP-' ) {
+	public function namespaced( $namespace = 'mc4wp-' ) {
+		// TODO: Find an easy way to make prefix case insensitive
 		return $this->request->params->all_with_prefix( $namespace );
 	}
 
