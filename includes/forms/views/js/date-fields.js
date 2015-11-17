da03d05779da85402bd377eb7b825fa51b49115a
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-17 10:48:58 +0700

    Update `date` pattern for older browsers.

--- includes/forms/views/js/date-fields.js
+++ includes/forms/views/js/date-fields.js
@@ -7,10 +7,10 @@ if( testInput.type !== 'date') {
 	var dateFields = document.querySelectorAll('.mc4wp-form input[type="date"]');
 	for(var i=0; i<dateFields.length; i++) {
 		if(!dateFields[i].placeholder) {
-			dateFields[i].placeholder = 'yyyy/mm/dd';
+			dateFields[i].placeholder = 'YYYY-MM-DD';
 		}
 		if(!dateFields[i].pattern) {
-			dateFields[i].pattern = '(?:19|20)[0-9]{2}/(?:(?:0[1-9]|1[0-2])/(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])/(?:30))|(?:(?:0[13578]|1[02])-31))';
+			dateFields[i].pattern = '^(?:19|20|21)\d\d[- \/.](?:0{0,1}[1-9]|1[012])[- \/.](?:0{0,1}[1-9]|[12][0-9]|3[01])$';
 		}
 	}
 }
