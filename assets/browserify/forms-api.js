f933c0b3a8f87b325c69e5dbca1561681ee646fd
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-24 08:50:30 +0700

    JS: Populate forms early in the request, but delay event triggering until `window.load` (so all other scripts get a chance to load).

--- assets/browserify/forms-api.js
+++ assets/browserify/forms-api.js
@@ -2,6 +2,12 @@
 
 var mc4wp = window.mc4wp || {};
 
+// bail early if we're on IE8..
+// TODO: just don't load in IE8
+if( ! window.addEventListener ) {
+	return;
+}
+
 // deps & vars
 var Gator = require('gator');
 var forms = require('./forms/forms.js');
@@ -9,41 +15,42 @@ var listeners = window.mc4wp && window.mc4wp.listeners ? window.mc4wp.listeners 
 var config = window.mc4wp_forms_config || {};
 
 // funcs
-function handleFormSubmission(formConfig){
-	var form,
-		element = document.getElementById(formConfig.element_id);
-
-	// get form by element, this might be a dummy element
-	form = forms.getByElement(element);
-
-	// add class & trigger event
+function triggerFormEvents(form,action,errors,data) {
+	// trigger events
 	forms.trigger( 'submitted', [form]);
 
-	// trigger eve
-	if( formConfig.errors ) {
-		// form has errors, repopulate it.
-		form.setData(formConfig.data);
-		forms.trigger('error', [form, formConfig.errors]);
+	if( errors ) {
+		forms.trigger('error', [form, errors]);
 	} else {
 		// form was successfully submitted
-		forms.trigger('success', [form, formConfig.data]);
-		forms.trigger(formConfig.action + "d", [form, formConfig.data]);
+		forms.trigger('success', [form, data]);
+		forms.trigger(action + "d", [form, data]);
+	}
+}
+
+function handleFormRequest(form,action,errors,data){
+
+	// get form by element, element might be null
+	var animate;
+
+	if( errors ) {
+		form.setData(data);
 	}
 
-	if( element && config.auto_scroll ) {
-		form.placeIntoView( (config.auto_scroll === 'animated') );
+	if( scroll ) {
+		animate = (scroll === 'animated');
+		form.placeIntoView(animate);
 	}
+
+	// trigger events on window.load so all other scripts have loaded
+	window.addEventListener('load', function(){
+		triggerFormEvents(form, action, errors, data);
+	})
 }
 
 // register early listeners
 for(var i=0; i<listeners.length;i++) {
 	forms.on(listeners[i].event, listeners[i].callback);
-}
-
-
-// was a form submitted?
-if( config.submitted_form && config.submitted_form.id ) {
-	handleFormSubmission(config.submitted_form);
 }
 
 // Bind browser events to form events (using delegation to work with AJAX loaded forms as well)
@@ -66,6 +73,14 @@ Gator(document.body).on('change', '.mc4wp-form', function(event) {
 	var form = forms.getByElement(event.target || event.srcElement);
 	forms.trigger('change', [form,event]);
 });
+
+if( config.submitted_form ) {
+	var formConfig = config.submitted_form,
+		element = document.getElementById(formConfig.element_id),
+		form = forms.getByElement(element);
+
+	handleFormRequest(form,formConfig.action, formConfig.errors,formConfig.data);
+}
 
 // expose forms object
 mc4wp.forms = forms;
