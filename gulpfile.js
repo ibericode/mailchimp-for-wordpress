206de2ff6587fe06e5c250f416cfbf77ee12bdb2
Danny van Kooten <dannyvankooten@gmail.com>
2015-11-30 15:28:37 +0700

    Remove per-project tasks in favor of global shell commands. See dannyvankooten/scripts.sh.

--- gulpfile.js
+++ gulpfile.js
@@ -17,21 +17,6 @@ var streamify = require('gulp-streamify');
 var globby = require('globby');
 var buffer = require('vinyl-buffer');
 var through = require('through2');
-var exec = require('child_process').exec;
-var runSequence = require('run-sequence');
-
-// CLI arguments
-var options = minimist(process.argv.slice(2), {
-	string: ['version', 'dest'],
-	default: {
-		version: false,
-		dest: false
-	},
-	alias: {
-		v: 'version',
-		d: 'dest'
-	}
-});
 
 gulp.task('default', ['sass', 'browserify']);
 
@@ -81,82 +66,6 @@ gulp.task('browserify', function () {
 	}).catch(function(err) {});
 
 	return bundledStream;
-});
-
-gulp.task('package', function(cb) {
-	var dirname = __dirname.split('/').pop();
-	var suffix = options.version ? '-' + options.version : '';
-	var dest =(  options.dest ? options.dest : '../' ) + dirname + suffix +'.zip';
-	exec( 'git archive master --format=zip --prefix='+ dirname +'/ --output=' + dest, function (err, stdout, stderr ) {
-		util.log("Package "+dest+" created!");
-		cb(err);
-	});
-});
-
-gulp.task('release', function(cb) {
-	runSequence([ 'languages', 'autoloader', 'bump-version'], 'package', cb);
-});
-
-gulp.task('languages', function(cb) {
-	exec( 'tx pull', function(err, stdout, stderr) {
-		console.log( stdout, stderr );
-		exec( 'for FILE in `find languages/. -name "*.po"`; do msgfmt -o ${FILE/.po/.mo} $FILE; done', function(err,stdout,stderr) {
-			console.log( stdout, stderr );
-			cb(err);
-		})
-	});
-});
-
-gulp.task('autoloader', function(cb) {
-
-	// remove dev-dependencies
-	fs.stat('vendor/phpunit', function (err, stats) {
-		var cmd = err ? '' : 'composer update --no-dev --prefer-dist';
-		exec( cmd, function(err,stdout,stderr) {
-			console.log( stdout, stderr );
-			exec('composer dump-autoload --optimize', function (err, stdout, stderr) {
-				console.log( stdout, stderr );
-				cb(err);
-			});
-		});
-	});
-});
-
-gulp.task('bump-version', function(cb) {
-
-	if( ! options.version ) {
-		util.log(util.colors.red("Please specify a --version argument."));
-		return;
-	}
-
-	// Bump version in readme.txt
-	var readme = gulp.src('./readme.txt', {base: './'})
-		.pipe(replace(/Stable tag: .*/i, 'Stable tag: ' + options.version))
-		.pipe(intercept(function(file) {
-			// Check if a Changelog section is present for this version
-			var regex = new RegExp('Changelog [\\s\\S]*\\=\\s' + options.version.replace('.', '\\.') + '\\s', '');
-			var match = file.contents.toString().match(regex);
-			if(! match) {
-				util.beep();
-				util.log(util.colors.red("readme.txt does not have a changelog for version " + options.version + " yet."));
-				var err = new util.PluginError('test', 'something broke');
-			}
-			return file;
-		}))
-		.pipe(gulp.dest('./'));
-
-	// Bump version in main plugin file.
-	var plugin = gulp.src('./mailchimp-for-wp.php', {base: './'})
-		.pipe(replace(/Version: .*/i, 'Version: ' + options.version))
-		.pipe(replace(/define\s*\(\s*['"]MC4WP_VERSION['"]\s*,.+/, "define( 'MC4WP_VERSION', '" + options.version + "' );"))
-		.pipe(gulp.dest('./'));
-
-	// Bunp version in composer.json
-	var composer = gulp.src('./*.json', { base: './' })
-		.pipe(replace(/\"version\"\:.*/i, '"version": "' + options.version + '",'))
-		.pipe(gulp.dest('./'));
-
-	return merge(plugin,readme, composer);
 });
 
 gulp.task('watch', function () {
