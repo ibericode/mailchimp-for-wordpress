c313c532df49a7ee4624b7a5f6aa029639ee276c
Danny van Kooten <dannyvankooten@gmail.com>
2015-12-01 12:31:45 +0700

    Add more unit tests for `MC4WP_Form` class.

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -145,5 +145,74 @@ class FormTest extends PHPUnit_Framework_TestCase {
 		self::assertEquals( $form->data, $data_uppercased );
 	}
 
+	/**
+	 * @covers MC4WP_Form::get_required_fields
+	 */
+	public function test_get_required_fields() {
+		$form = new MC4WP_Form(15);
+		$form->settings['required_fields'] = 'EMAIL';
+		self::assertEquals( $form->get_required_fields(), array() );
+
+		// use array_diff here as order of elements is not important
+		$form->settings['required_fields'] = 'EMAIL,FNAME';
+		self::assertEmpty( array_diff( $form->get_required_fields(), array( 'FNAME' ) ) );
+
+		$form->settings['required_fields'] = 'website,EMAIL,FNAME';
+		self::assertEmpty( array_diff( $form->get_required_fields(), array( 'WEBSITE', 'FNAME' ) ) );
+	}
+
+	/**
+	 * @covers MC4WP_Form::get_stylesheet
+	 */
+	public function test_get_stylesheet() {
+		$form = new MC4WP_Form(15);
+		$form->settings['css'] = false;
+		self::assertEmpty( $form->get_stylesheet() );
+
+		$form->settings['css'] = 'some-stylesheet';
+		self::assertEquals( $form->get_stylesheet(), 'some-stylesheet' );
+
+		// themes are bundled
+		$form->settings['css'] = 'theme-something';
+		self::assertEquals( $form->get_stylesheet(), 'themes' );
+	}
+
+	/**
+	 * @covers MC4WP_Form::add_error
+	 * @covers MC4WP_Form::has_errors
+	 */
+	public function test_errors() {
+		$form = new MC4WP_Form(15);
+
+		self::assertFalse( $form->has_errors() );
+
+		$form->add_error( 'some_error' );
+		self::assertTrue( $form->has_errors() );
+
+		$form->add_error( 'some_other_error' );
+		self::assertCount( 2, $form->errors );
+	}
+
+	/**
+	 * @covers MC4WP_Form::get_message
+	 */
+	public function test_get_message() {
+		$form = new MC4WP_Form(15);
+
+
+		$errorMessage = new MC4WP_Form_Message( 'Error text', 'error' );
+		$successMessage = new MC4WP_Form_Message( 'Success text', 'success' );
+
+		$form->messages = array(
+			'error' => $errorMessage,
+			'success' => $successMessage
+		);
+
+		self::assertInstanceOf( 'MC4WP_Form_Message', $form->get_message( 'error' ) );
+		self::assertEquals( $errorMessage, $form->get_message( 'error' ) );
+		self::assertEquals( $errorMessage, $form->get_message( 'unexisting_error' ) );
+		self::assertEquals( $successMessage, $form->get_message( 'success' ) );
+	}
+
 
 }
