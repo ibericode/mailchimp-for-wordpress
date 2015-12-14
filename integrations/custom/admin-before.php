66b012dc028e51b2125e48eeef99e568c176cbb2
Danny van Kooten <dannyvankooten@gmail.com>
2015-12-14 12:24:36 +0700

    Make sure textarea with code sample adjusts to actual number of lines in code snippet.

--- integrations/custom/admin-before.php
+++ integrations/custom/admin-before.php
@@ -2,10 +2,14 @@
 	<?php _e( 'To get a custom integration to work, include the following HTML in the form you are trying to integrate with.', 'mailchimp-for-wp' ); ?>
 </p>
 
-<textarea style="width: 100%;" class="code-sample" rows="5" readonly onfocus="this.select()"><?php ob_start(); ?><p>
+<?php ob_start(); ?>
+<p>
 	<label>
 		<input type="checkbox" name="mc4wp-subscribe" value="1" />
-		Subscribe to our newsletter.
+		<?php _e( 'Subscribe to our newsletter.', 'mailchimp-for-wp' ); ?>
 	</label>
-</p><?php $html = ob_get_clean(); echo esc_textarea( $html ); ?>
-</textarea>
+</p>
+
+<?php $html = ob_get_clean(); ?>
+
+<textarea class="widefat code-sample" rows="<?php echo substr_count( $html, PHP_EOL ); ?>" readonly onfocus="this.select()"><?php echo esc_textarea( $html ); ?></textarea>
