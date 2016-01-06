f948d0a0c2a8a6971a9fa6addaca14decd352135
Danny van Kooten <dannyvankooten@gmail.com>
2016-01-06 15:15:28 +0700

    Run default filter early so they can be easily overridden.

--- includes/default-filters.php
+++ includes/default-filters.php
@@ -5,4 +5,4 @@ defined( 'ABSPATH' ) or exit;
 add_filter( 'mc4wp_form_merge_vars', 'mc4wp_guess_merge_vars' );
 add_filter( 'mc4wp_integration_merge_vars', 'mc4wp_guess_merge_vars' );
 
-add_filter( 'mc4wp_use_sslverify', '__mc4wp_use_sslverify' );
+add_filter( 'mc4wp_use_sslverify', '__mc4wp_use_sslverify', 1 );
