c4c80c2bb249dc70063beaeb94258994df7e77a1
Danny van Kooten <dannyvankooten@gmail.com>
2016-04-12 11:55:24 +0200

    When upgrading to 4.0, build a map of current groupings ID to new interest category ID's in order to be backwards compatible.

---
+++ includes/admin/migrations/4.0.0-groupings-to-interests.php
@@ -0,0 +1,54 @@
+<?php
+
+defined( 'ABSPATH' ) or exit;
+
+$options = get_option( 'mc4wp', array() );
+if( empty( $options['api_key'] ) ) {
+    return;
+}
+
+$api_v3 = new MC4WP_API_v3( $options['api_key'] );
+
+// get current state from transient
+$lists = get_transient( 'mc4wp_mailchimp_lists_fallback' );
+
+// for testing, store in a transient we'll never touch
+set_transient( 'mc4wp_old_mailchimp_list_data', $lists, WEEK_IN_SECONDS );
+
+if( empty( $lists ) ) {
+    return;
+}
+
+$api_v3 = new MC4WP_API_v3( $options['api_key'] );
+$map = array();
+
+foreach( $lists as $list ) {
+
+    // no groupings? easy!
+    if( empty( $list->groupings ) ) {
+        continue;
+    }
+
+    // prepare groupings in id => name format
+    $groupings = array();
+    foreach( $list->groupings as $grouping ) {
+        $grouping = (array) $grouping;
+        $groupings[ "" . $grouping['id'] ] = $grouping['name'];
+    }
+
+    // fetch (new) interest categories for this list
+    $interests = $api_v3->get_list_interest_categories( $list->id );
+
+    foreach( $interests as $interest ) {
+        $grouping_id = array_search( $interest->title, $groupings );
+        if( $grouping_id ) {
+            $map[ (string) $grouping_id ] = $interest->id;
+        }
+    }
+
+}
+
+if( ! empty( $map ) ) {
+    update_option( 'mc4wp_groupings_map', $map );
+}
+
