2c472ea74b88ac69ec1bb344751ba4e75331c11b
Danny van Kooten <dannyvankooten@gmail.com>
2016-04-12 15:16:43 +0200

    Update copyright year in main plugin file.

--- mailchimp-for-wp.php
+++ mailchimp-for-wp.php
@@ -11,7 +11,7 @@ Domain Path: /languages
 License: GPL v3
 
 MailChimp for WordPress
-Copyright (C) 2012-2015, Danny van Kooten, hi@dannyvankooten.com
+Copyright (C) 2012-2016, Danny van Kooten, hi@dannyvankooten.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
