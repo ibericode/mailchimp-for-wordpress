58bfe932131f6c1ba073260ca5fdd5ec2e6fce6b
Danny van Kooten <dannyvankooten@gmail.com>
2016-05-27 12:30:04 +0200

    Add `updated` message.

--- config/default-form-messages.php
+++ config/default-form-messages.php
@@ -4,6 +4,18 @@ return array(
 		'type' => 'success',
 		'text' => __( 'Thank you, your sign-up request was successful! Please check your email inbox to confirm.', 'mailchimp-for-wp' )
 	),
+	'updated' 				   => array(
+		'type' => 'success',
+		'text' => __( 'Thank you, your records have been updated!', 'mailchimp-for-wp' ),
+	),
+	'unsubscribed'             => array(
+		'type' => 'success',
+		'text' => __( 'You were successfully unsubscribed.', 'mailchimp-for-wp' ),
+	),
+	'not_subscribed'           => array(
+		'type' => 'notice',
+		'text' => __( 'Given email address is not subscribed.', 'mailchimp-for-wp' ),
+	),
 	'error'                    => array(
 		'type' => 'error',
 		'text' => __( 'Oops. Something went wrong. Please try again later.', 'mailchimp-for-wp' ),
@@ -20,14 +32,7 @@ return array(
 		'type' => 'error',
 		'text' => __( 'Please fill in the required fields.', 'mailchimp-for-wp' ),
 	),
-	'unsubscribed'             => array(
-		'type' => 'success',
-		'text' => __( 'You were successfully unsubscribed.', 'mailchimp-for-wp' ),
-	),
-	'not_subscribed'           => array(
-		'type' => 'notice',
-		'text' => __( 'Given email address is not subscribed.', 'mailchimp-for-wp' ),
-	),
+	
 	'no_lists_selected'        => array(
 		'type' => 'error',
 		'text' => __( 'Please select at least one list.', 'mailchimp-for-wp' )
