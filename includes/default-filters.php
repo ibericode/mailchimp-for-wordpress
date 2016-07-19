eb61fccc9f989b411394af24833c8c20065cd3cb
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-19 15:14:07 +0200

    Update old style groupings array for MailChimp Top Bar as well, using new `mctb_data` filter.

--- includes/default-filters.php
+++ includes/default-filters.php
@@ -5,6 +5,7 @@ defined( 'ABSPATH' ) or exit;
 add_filter( 'mc4wp_form_data', 'mc4wp_add_name_data', 60 );
 add_filter( 'mc4wp_integration_data', 'mc4wp_add_name_data', 60 );
 
+add_filter( 'mctb_data', 'mc4wp_update_groupings_data', 90 );
 add_filter( 'mc4wp_form_data', 'mc4wp_update_groupings_data', 90 );
 add_filter( 'mc4wp_integration_data', 'mc4wp_update_groupings_data', 90 );
 
