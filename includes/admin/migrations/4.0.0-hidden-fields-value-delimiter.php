7398147968b8d307bfa561fb4a68789cee39b8ea
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-20 15:17:35 +0200

    Add migration for new field value delimiter. See #302

---
+++ includes/admin/migrations/4.0.0-hidden-fields-value-delimiter.php
@@ -0,0 +1,26 @@
+<?php
+
+defined( 'ABSPATH' ) or exit;
+
+/** @ignore */
+function __mc4wp_400_replace_comma_with_pipe( $matches ) {
+    $old = $matches[1];
+    $new = str_replace( ',', '|', $old );
+    return str_replace( $old, $new, $matches[0] );
+}
+
+// get all forms
+$posts = get_posts( array( 'post_type' => 'mc4wp-form', 'numberposts' => -1 ) );
+
+foreach( $posts as $post ) {
+
+    // find hidden field values in form and pass through replace function
+    $old = $post->post_content;
+    $new = preg_replace_callback( '/type="hidden" .* value="(.*)"/i', '__mc4wp_400_replace_comma_with_pipe', $old );
+
+    // update post if we replaced something
+    if( $new != $old ) {
+        $post->post_content = $new;
+        wp_update_post( $post );
+    }
+}
