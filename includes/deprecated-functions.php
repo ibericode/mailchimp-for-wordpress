b37364bf98d29ca3ad5ed0ea4caa5ad7584f29fa
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-28 11:54:54 +0200

    Call function by the correct name.

--- includes/deprecated-functions.php
+++ includes/deprecated-functions.php
@@ -11,7 +11,7 @@
 function mc4wp_guess_merge_vars( $merge_vars = array() ) {
 	_deprecated_function( __FUNCTION__, 'MailChimp for WordPress v4.0' );
 	$merge_vars = mc4wp_add_name_data( $merge_vars );
-	$merge_vars = mc4wp_update_groupings_data( $merge_vars );
+	$merge_vars = __mc4wp_update_groupings_data( $merge_vars );
 	return $merge_vars;
 }
 
@@ -38,7 +38,7 @@ function mc4wp_checkbox() {
  * @param int $id
  * @param array $attributes
  *
- * @return MC4WP_Form
+ * @return string
  *
  */
 function mc4wp_form( $id = 0, $attributes = array() ) {
