607b7e2c1e4197018e28161a1fc1cb6f1ee992c3
Danny van Kooten <dannyvankooten@gmail.com>
2016-08-10 09:32:14 +0200

    Remove update opt-in for 4.x, as we're there already.

--- includes/admin/class-admin.php
+++ includes/admin/class-admin.php
@@ -29,11 +29,6 @@ class MC4WP_Admin {
 	protected $ads;
 
 	/**
-	 * @var MC4WP_Update_Optin
-	 */
-	protected $update_optin;
-
-	/**
 	 * Constructor
 	 *
 	 * @param MC4WP_Admin_Messages $messages
@@ -45,9 +40,6 @@ class MC4WP_Admin {
 		$this->plugin_file = plugin_basename( MC4WP_PLUGIN_FILE );
 		$this->ads = new MC4WP_Admin_Ads();
 		$this->load_translations();
-
-		// update opt-in
-		$this->update_optin = new MC4WP_Update_Optin( '4.0.0', $this->plugin_file, MC4WP_PLUGIN_DIR . 'includes/views/parts/update-4.x-notice.php' );
 	}
 
 	/**
@@ -70,7 +62,6 @@ class MC4WP_Admin {
 
 		$this->ads->add_hooks();
 		$this->messages->add_hooks();
-		$this->update_optin->add_hooks();
 	}
 
 	/**
