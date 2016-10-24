fa369916fa217d47e58e0bf075c30cb1ffe947ab
Danny van Kooten <dannyvankooten@gmail.com>
2016-10-24 14:50:04 +0200

    Sort default integration options alphabetically and add default for `wrap_p`

--- config/default-integration-options.php
+++ config/default-integration-options.php
@@ -1,12 +1,13 @@
 <?php
 return array(
+    'css'           => 0,
+    'double_optin'  => 1,
 	'enabled'       => 0,
 	'implicit'      => 0,
 	'label'         => __( 'Sign me up for the newsletter!', 'mailchimp-for-wp' ),
+    'lists'         => array(),
 	'precheck'      => 1,
-	'css'           => 0,
-	'lists'         => array(),
-	'double_optin'  => 1,
+    'replace_interests' => 0,
 	'update_existing' => 0,
-	'replace_interests' => 0,
+    'wrap_p' => 1
 );
