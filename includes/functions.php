db012c21f06fc227bed9d177dd70a4b93d019818
Danny van Kooten <dannyvankooten@gmail.com>
2017-02-08 11:25:20 +0100

    formatting

--- includes/functions.php
+++ includes/functions.php
@@ -18,18 +18,19 @@
  *
  * @throws Exception when service is not found
  */
-function mc4wp( $service = null ) {
-	static $mc4wp;
+function mc4wp($service = null)
+{
+    static $mc4wp;
 
-	if( ! $mc4wp ) {
-		$mc4wp = new MC4WP_Container();
-	}
+    if (!$mc4wp) {
+        $mc4wp = new MC4WP_Container();
+    }
 
-	if( $service ) {
-		return $mc4wp->get( $service );
-	}
+    if ($service) {
+        return $mc4wp->get($service);
+    }
 
-	return $mc4wp;
+    return $mc4wp;
 }
 
 /**
@@ -41,21 +42,22 @@ function mc4wp( $service = null ) {
  * @static array $options
  * @return array
  */
-function mc4wp_get_options() {
-	static $options;
+function mc4wp_get_options()
+{
+    static $options;
 
-	if( ! $options ) {
-		$defaults = require MC4WP_PLUGIN_DIR . 'config/default-settings.php';
-		$options = (array) get_option( 'mc4wp', array() );
-		$options = array_merge( $defaults, $options );
-	}
+    if (!$options) {
+        $defaults = require MC4WP_PLUGIN_DIR . 'config/default-settings.php';
+        $options = (array)get_option('mc4wp', array());
+        $options = array_merge($defaults, $options);
+    }
 
-	/**
-	 * Filters the MailChimp for WordPress settings (general).
-	 *
-	 * @param array $options
-	 */
-	return apply_filters( 'mc4wp_settings', $options );
+    /**
+     * Filters the MailChimp for WordPress settings (general).
+     *
+     * @param array $options
+     */
+    return apply_filters('mc4wp_settings', $options);
 }
 
 /**
@@ -66,10 +68,11 @@ function mc4wp_get_options() {
  *
  * @return MC4WP_API_v3
  */
-function mc4wp_get_api_v3() {
-	$opts = mc4wp_get_options();
-	$instance = new MC4WP_API_v3( $opts['api_key'] );
-	return $instance;
+function mc4wp_get_api_v3()
+{
+    $opts = mc4wp_get_options();
+    $instance = new MC4WP_API_v3($opts['api_key']);
+    return $instance;
 }
 
 /**
@@ -83,11 +86,12 @@ function mc4wp_get_api_v3() {
  *
  * @return MC4WP_API
  */
-function mc4wp_get_api() {
-	_deprecated_function( __FUNCTION__, '4.0', 'mc4wp_get_api_v3' );
-	$opts = mc4wp_get_options();
-	$instance = new MC4WP_API( $opts['api_key'] );
-	return $instance;
+function mc4wp_get_api()
+{
+    _deprecated_function(__FUNCTION__, '4.0', 'mc4wp_get_api_v3');
+    $opts = mc4wp_get_options();
+    $instance = new MC4WP_API($opts['api_key']);
+    return $instance;
 }
 
 /**
@@ -95,30 +99,31 @@ function mc4wp_get_api() {
  *
  * @return MC4WP_Debug_Log
  */
-function mc4wp_get_debug_log() {
+function mc4wp_get_debug_log()
+{
     $opts = mc4wp_get_options();
 
-	// get default log file location
-	$upload_dir = wp_upload_dir( null, false );
-	$file = trailingslashit( $upload_dir['basedir'] ) . 'mc4wp-debug-log.php';
+    // get default log file location
+    $upload_dir = wp_upload_dir(null, false);
+    $file = trailingslashit($upload_dir['basedir']) . 'mc4wp-debug-log.php';
 
-	/**
-	 * Filters the log file to write to.
-	 *
-	 * @param string $file The log file location. Default: /wp-content/uploads/mc4wp-debug.log
-	 */
-	$file = apply_filters( 'mc4wp_debug_log_file', $file );
+    /**
+     * Filters the log file to write to.
+     *
+     * @param string $file The log file location. Default: /wp-content/uploads/mc4wp-debug.log
+     */
+    $file = apply_filters('mc4wp_debug_log_file', $file);
 
-	/**
-	 * Filters the minimum level to log messages.
-	 *
-	 * @see MC4WP_Debug_Log
-	 *
-	 * @param string|int $level The minimum level of messages which should be logged.
-	 */
-	$level = apply_filters( 'mc4wp_debug_log_level', $opts['debug_log_level'] );
+    /**
+     * Filters the minimum level to log messages.
+     *
+     * @see MC4WP_Debug_Log
+     *
+     * @param string|int $level The minimum level of messages which should be logged.
+     */
+    $level = apply_filters('mc4wp_debug_log_level', $opts['debug_log_level']);
 
-	return new MC4WP_Debug_Log( $file, $level );
+    return new MC4WP_Debug_Log($file, $level);
 }
 
 /**
@@ -129,23 +134,24 @@ function mc4wp_get_debug_log() {
  *
  * @return  string  The current URL (escaped)
  */
-function mc4wp_get_current_url() {
+function mc4wp_get_current_url()
+{
 
-	global $wp;
+    global $wp;
 
-	// get requested url from global $wp object
-	$site_request_uri = $wp->request;
+    // get requested url from global $wp object
+    $site_request_uri = $wp->request;
 
-	// fix for IIS servers using index.php in the URL
-	if( false !== stripos( $_SERVER['REQUEST_URI'], '/index.php/' . $site_request_uri ) ) {
-		$site_request_uri = 'index.php/' . $site_request_uri;
-	}
+    // fix for IIS servers using index.php in the URL
+    if (false !== stripos($_SERVER['REQUEST_URI'], '/index.php/' . $site_request_uri)) {
+        $site_request_uri = 'index.php/' . $site_request_uri;
+    }
 
-	// concatenate request url to home url
-	$url = home_url( $site_request_uri );
-	$url = trailingslashit( $url );
+    // concatenate request url to home url
+    $url = home_url($site_request_uri);
+    $url = trailingslashit($url);
 
-	return esc_url( $url );
+    return esc_url($url);
 }
 
 /**
@@ -156,24 +162,25 @@ function mc4wp_get_current_url() {
  *
  * @return mixed
  */
-function mc4wp_sanitize_deep( $value ) {
+function mc4wp_sanitize_deep($value)
+{
 
-	if ( is_scalar( $value ) ) {
-	    // strip all HTML tags & whitespace
-		$value = trim( strip_tags( $value ) );
+    if (is_scalar($value)) {
+        // strip all HTML tags & whitespace
+        $value = trim(strip_tags($value));
 
-		// convert &amp; back to &
-        $value = html_entity_decode( $value, ENT_NOQUOTES );
-	} elseif( is_array( $value ) ) {
-		$value = array_map( 'mc4wp_sanitize_deep', $value );
-	} elseif ( is_object($value) ) {
-		$vars = get_object_vars( $value );
-		foreach ( $vars as $key => $data ) {
-			$value->{$key} = mc4wp_sanitize_deep( $data );
-		}
-	}
+        // convert &amp; back to &
+        $value = html_entity_decode($value, ENT_NOQUOTES);
+    } elseif (is_array($value)) {
+        $value = array_map('mc4wp_sanitize_deep', $value);
+    } elseif (is_object($value)) {
+        $vars = get_object_vars($value);
+        foreach ($vars as $key => $data) {
+            $value->{$key} = mc4wp_sanitize_deep($data);
+        }
+    }
 
-	return $value;
+    return $value;
 }
 
 /**
@@ -184,78 +191,79 @@ function mc4wp_sanitize_deep( $value ) {
  * @param array $data
  * @return array
  */
-function _mc4wp_update_groupings_data( $data = array() ) {
+function _mc4wp_update_groupings_data($data = array())
+{
 
     // data still has old "GROUPINGS" key?
-	if( empty( $data['GROUPINGS'] ) ) {
+    if (empty($data['GROUPINGS'])) {
         return $data;
     }
 
     // prepare new key
-    if( ! isset( $data['INTERESTS'] ) ) {
+    if (!isset($data['INTERESTS'])) {
         $data['INTERESTS'] = array();
     }
 
-    $map = get_option( 'mc4wp_groupings_map', array() );
+    $map = get_option('mc4wp_groupings_map', array());
 
-    foreach( $data['GROUPINGS'] as $grouping_id => $groups ) {
+    foreach ($data['GROUPINGS'] as $grouping_id => $groups) {
 
         // for compatibility with expanded grouping arrays
         $grouping_key = $grouping_id;
-        if( is_array( $groups ) && isset( $groups['id'] ) && isset( $groups['groups'] ) ) {
+        if (is_array($groups) && isset($groups['id']) && isset($groups['groups'])) {
             $grouping_id = $groups['id'];
             $groups = $groups['groups'];
         }
 
         // do we have transfer data for this grouping id?
-        if( ! isset( $map[ $grouping_id ] ) ) {
+        if (!isset($map[$grouping_id])) {
             continue;
         }
 
         // if we get a string, explode on delimiter(s)
-        if( is_string( $groups ) ) {
+        if (is_string($groups)) {
             // for BC with 3.x: explode on comma's
-            $groups = join( '|', explode(',', $groups ) );
+            $groups = join('|', explode(',', $groups));
 
             // explode on current delimiter
-            $groups = explode( '|', $groups );
+            $groups = explode('|', $groups);
         }
 
         // loop through groups and find interest ID
         $migrated = 0;
-        foreach( $groups as $key => $group_name_or_id ) {
+        foreach ($groups as $key => $group_name_or_id) {
 
             // do we know the new interest ID?
-            if( empty( $map[ $grouping_id ]['groups'][ $group_name_or_id ] ) ) {
+            if (empty($map[$grouping_id]['groups'][$group_name_or_id])) {
                 continue;
             }
 
-            $interest_id = $map[ $grouping_id ]['groups'][ $group_name_or_id ];
+            $interest_id = $map[$grouping_id]['groups'][$group_name_or_id];
 
             // add to interests data
-            if( ! in_array( $interest_id, $data['INTERESTS'] ) ) {
+            if (!in_array($interest_id, $data['INTERESTS'])) {
                 $migrated++;
                 $data['INTERESTS'][] = $interest_id;
             }
         }
-        
+
         // remove old grouping ID if we migrated all groups.
-        if( $migrated === count( $groups ) ) {
-            unset( $data['GROUPINGS'][$grouping_key] );
+        if ($migrated === count($groups)) {
+            unset($data['GROUPINGS'][$grouping_key]);
         }
     }
 
-	// if everything went well, this is now empty & moved to new INTERESTS key.
-	if( empty( $data['GROUPINGS'] ) ) {
-		unset( $data['GROUPINGS'] );
-	}
-
-	// is this empty? just unset it then.
-	if( empty( $data['INTERESTS'] ) ) {
-	    unset( $data['INTERESTS'] );
+    // if everything went well, this is now empty & moved to new INTERESTS key.
+    if (empty($data['GROUPINGS'])) {
+        unset($data['GROUPINGS']);
     }
-	
-	return $data;
+
+    // is this empty? just unset it then.
+    if (empty($data['INTERESTS'])) {
+        unset($data['INTERESTS']);
+    }
+
+    return $data;
 }
 
 /**
@@ -268,27 +276,28 @@ function _mc4wp_update_groupings_data( $data = array() ) {
  *
  * @return array
  */
-function mc4wp_add_name_data( $data = array() ) {
+function mc4wp_add_name_data($data = array())
+{
 
-	// Guess first and last name
-	if ( ! empty( $data['NAME'] ) && empty( $data['FNAME'] ) && empty( $data['LNAME'] ) ) {
-        $data['NAME'] = trim( $data['NAME'] );
-		$strpos = strpos( $data['NAME'], ' ' );
+    // Guess first and last name
+    if (!empty($data['NAME']) && empty($data['FNAME']) && empty($data['LNAME'])) {
+        $data['NAME'] = trim($data['NAME']);
+        $strpos = strpos($data['NAME'], ' ');
 
-		if ( $strpos !== false ) {
-			$data['FNAME'] = trim( substr( $data['NAME'], 0, $strpos ) );
-			$data['LNAME'] = trim( substr( $data['NAME'], $strpos ) );
-		} else {
-			$data['FNAME'] = $data['NAME'];
-		}
-	}
+        if ($strpos !== false) {
+            $data['FNAME'] = trim(substr($data['NAME'], 0, $strpos));
+            $data['LNAME'] = trim(substr($data['NAME'], $strpos));
+        } else {
+            $data['FNAME'] = $data['NAME'];
+        }
+    }
 
-	// Set name value
-	if( empty( $data['NAME'] ) && ! empty( $data['FNAME'] ) && ! empty( $data['LNAME'] ) ) {
-		$data['NAME'] = sprintf( '%s %s', $data['FNAME'], $data['LNAME'] );
-	}
-	
-	return $data;
+    // Set name value
+    if (empty($data['NAME']) && !empty($data['FNAME']) && !empty($data['LNAME'])) {
+        $data['NAME'] = sprintf('%s %s', $data['FNAME'], $data['LNAME']);
+    }
+
+    return $data;
 }
 
 /**
@@ -301,18 +310,19 @@ function mc4wp_add_name_data( $data = array() ) {
  *
  * @return string
  */
-function mc4wp_get_email_type() {
+function mc4wp_get_email_type()
+{
 
-	$email_type = 'html';
+    $email_type = 'html';
 
-	/**
-	 * Filters the email type preference for this new subscriber.
-	 *
-	 * @param string $email_type
-	 */
-	$email_type = (string) apply_filters( 'mc4wp_email_type', $email_type );
+    /**
+     * Filters the email type preference for this new subscriber.
+     *
+     * @param string $email_type
+     */
+    $email_type = (string)apply_filters('mc4wp_email_type', $email_type);
 
-	return $email_type;
+    return $email_type;
 }
 
 /**
@@ -320,26 +330,27 @@ function mc4wp_get_email_type() {
  * @ignore
  * @return bool
  */
-function _mc4wp_use_sslverify() {
+function _mc4wp_use_sslverify()
+{
 
-	// Disable for all transports other than CURL
-	if( ! function_exists( 'curl_version' ) ) {
-		return false;
-	}
+    // Disable for all transports other than CURL
+    if (!function_exists('curl_version')) {
+        return false;
+    }
 
-	$curl = curl_version();
+    $curl = curl_version();
 
-	// Disable if OpenSSL is not installed
-	if( empty( $curl['ssl_version'] ) ) {
-		return false;
-	}
+    // Disable if OpenSSL is not installed
+    if (empty($curl['ssl_version'])) {
+        return false;
+    }
 
-	// Disable if on WP 4.4, see https://core.trac.wordpress.org/ticket/34935
-	if( $GLOBALS['wp_version'] === '4.4' ) {
-		return false;
-	}
+    // Disable if on WP 4.4, see https://core.trac.wordpress.org/ticket/34935
+    if ($GLOBALS['wp_version'] === '4.4') {
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 /**
@@ -348,22 +359,24 @@ function _mc4wp_use_sslverify() {
  * @param string $string
  * @return string
  */
-function mc4wp_obfuscate_string( $string ) {
-	$length = strlen( $string );
-	$obfuscated_length = ceil( $length / 2 );
-	$string = str_repeat( '*', $obfuscated_length ) . substr( $string, $obfuscated_length );
-	return $string;
+function mc4wp_obfuscate_string($string)
+{
+    $length = strlen($string);
+    $obfuscated_length = ceil($length / 2);
+    $string = str_repeat('*', $obfuscated_length) . substr($string, $obfuscated_length);
+    return $string;
 }
 
 /**
  * @internal
  * @ignore
  */
-function _mc4wp_obfuscate_email_addresses_callback( $m ) {
-    $one = $m[1] . str_repeat( '*', strlen( $m[2] ) );
-    $two = $m[3] . str_repeat( '*', strlen( $m[4] ) );
+function _mc4wp_obfuscate_email_addresses_callback($m)
+{
+    $one = $m[1] . str_repeat('*', strlen($m[2]));
+    $two = $m[3] . str_repeat('*', strlen($m[4]));
     $three = $m[5];
-    return sprintf( '%s@%s.%s', $one, $two, $three );
+    return sprintf('%s@%s.%s', $one, $two, $three);
 }
 
 /**
@@ -372,8 +385,9 @@ function _mc4wp_obfuscate_email_addresses_callback( $m ) {
  * @param $string String possibly containing email address
  * @return string
  */
-function mc4wp_obfuscate_email_addresses( $string ) {
-    return preg_replace_callback( '/([\w\.]{1,4})([\w\.]*)\@(\w{1,2})(\w*)\.(\w+)/', '_mc4wp_obfuscate_email_addresses_callback', $string );
+function mc4wp_obfuscate_email_addresses($string)
+{
+    return preg_replace_callback('/([\w\.]{1,4})([\w\.]*)\@(\w{1,2})(\w*)\.(\w+)/', '_mc4wp_obfuscate_email_addresses_callback', $string);
 }
 
 /**
@@ -381,7 +395,8 @@ function mc4wp_obfuscate_email_addresses( $string ) {
  *
  * @return void
  */
-function mc4wp_refresh_mailchimp_lists() {
+function mc4wp_refresh_mailchimp_lists()
+{
     $mailchimp = new MC4WP_MailChimp();
     $mailchimp->fetch_lists();
 }
