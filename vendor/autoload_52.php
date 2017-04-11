58dfa38c12d661de0b5f682e0625bc2601c8d32d
Danny van Kooten <dannyvankooten@gmail.com>
2017-04-11 11:30:50 +0200

    updated autoloader

--- vendor/autoload_52.php
+++ vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitf9f59db4e710aee97a4e75e7ce25e286::getLoader();
+return ComposerAutoloaderInitef76aa1bb6f3ca98f87763d7c82e66ab::getLoader();
