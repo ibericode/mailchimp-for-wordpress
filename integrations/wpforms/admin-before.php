fb49d46f39a4f18f0f54a90028939066d1e5373d
Danny <dannyvankooten@gmail.com>
2017-08-31 14:17:58 +0200

    add text to integration settings page

---
+++ integrations/wpforms/admin-before.php
@@ -0,0 +1,3 @@
+<p>
+	<?php printf( __( 'Use this integration by adding the "MailChimp" field to <a href="%s">your WPForms forms</a>.', 'mailchimp-for-wp' ), admin_url( 'admin.php?page=wpforms-overview' ) ); ?>
+</p>
