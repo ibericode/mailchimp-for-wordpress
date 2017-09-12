6c434f25c5679f3c3594e5faf7cc879c93b032fd
Danny <dannyvankooten@gmail.com>
2017-09-12 11:52:33 +0200

    change bootstrap method for Ninja Forms integration because of early plugins_loaded hook. fixes #476

--- integrations/ninja-forms/bootstrap.php
+++ integrations/ninja-forms/bootstrap.php
@@ -2,15 +2,8 @@
 
 mc4wp_register_integration( 'ninja-forms', 'MC4WP_Ninja_Forms_Integration', true );
 
-function _mc4wp_ninja_forms_register_field( $fields ) {
-    $fields['mc4wp_optin'] = new MC4WP_Ninja_Forms_Field();
-    return $fields;
+if( class_exists( 'Ninja_Forms' ) ) {
+    $ninja_forms = Ninja_Forms::instance();
+    $ninja_forms->fields['mc4wp_optin'] = new MC4WP_Ninja_Forms_Field();
+    $ninja_forms->actions['mc4wp_subscribe'] = new MC4WP_Ninja_Forms_Action();
 }
-
-function _mc4wp_ninja_forms_register_action( $actions ) {
-    $actions['mc4wp_subscribe'] = new MC4WP_Ninja_Forms_Action();
-    return $actions;
-}
-
-add_filter( 'ninja_forms_register_fields', '_mc4wp_ninja_forms_register_field' );
-add_filter( 'ninja_forms_register_actions', '_mc4wp_ninja_forms_register_action' );
