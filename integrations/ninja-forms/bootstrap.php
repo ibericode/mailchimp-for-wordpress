27d9cfd0c8654c8907d0ff78b30b117d6d1f259e
Danny <dannyvankooten@gmail.com>
2017-09-12 13:44:30 +0200

    check if property exists before bootstrapping, since we perform no other version checks

--- integrations/ninja-forms/bootstrap.php
+++ integrations/ninja-forms/bootstrap.php
@@ -2,8 +2,14 @@
 
 mc4wp_register_integration( 'ninja-forms', 'MC4WP_Ninja_Forms_Integration', true );
 
-if( class_exists( 'Ninja_Forms' ) ) {
+if( class_exists( 'Ninja_Forms' ) && method_exists( 'Ninja_Forms', 'instance' ) ) {
     $ninja_forms = Ninja_Forms::instance();
-    $ninja_forms->fields['mc4wp_optin'] = new MC4WP_Ninja_Forms_Field();
-    $ninja_forms->actions['mc4wp_subscribe'] = new MC4WP_Ninja_Forms_Action();
+
+    if( isset( $ninja_forms->fields ) ) {
+        $ninja_forms->fields['mc4wp_optin'] = new MC4WP_Ninja_Forms_Field();
+    }
+
+    if( isset( $ninja_forms->actions ) ) {
+        $ninja_forms->actions['mc4wp_subscribe'] = new MC4WP_Ninja_Forms_Action();
+    }
 }
