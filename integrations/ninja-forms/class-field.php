3789fc84f2b46c22f2211d0bddcb69a3c904fa88
Danny <dannyvankooten@gmail.com>
2018-02-14 12:22:04 +0100

    do not extend base checkbox class in ninja forms integration, to prevent double filter. fixes #533

--- integrations/ninja-forms/class-field.php
+++ integrations/ninja-forms/class-field.php
@@ -3,7 +3,7 @@
 /**
  * Class MC4WP_Ninja_Forms_Field
  */
-class MC4WP_Ninja_Forms_Field extends NF_Fields_Checkbox
+class MC4WP_Ninja_Forms_Field extends NF_Abstracts_Input
 {
     protected $_name = 'mc4wp_optin';
 
@@ -11,9 +11,53 @@ class MC4WP_Ninja_Forms_Field extends NF_Fields_Checkbox
 
     protected $_section = 'misc';
 
-    public function __construct() {
+    protected $_type = 'checkbox';
+
+    protected $_icon = 'check-square-o';
+
+    protected $_templates = 'checkbox';
+
+    protected $_test_value = 0;
+
+    protected $_settings =  array( 'checkbox_default_value', 'checked_calc_value', 'unchecked_calc_value' );
+
+    protected $_settings_exclude = array( 'default', 'placeholder', 'input_limit_set', 'checkbox_values' );
+
+    /**
+     * NF_Fields_Checkbox constructor.
+     * @since 3.0
+     */
+    public function __construct()
+    {
         parent::__construct();
 
         $this->_nicename = __( 'MailChimp opt-in', 'mailchimp-for-wp' );
+
+        $this->_settings[ 'label_pos' ][ 'value' ] = 'right';
+    }
+
+    /**
+     * Admin Form Element
+     * Display the checkbox on the edit submissions area.
+     * @since 3.0
+     *
+     * @param $id Field ID.
+     * @param $value Field value.
+     * @return string HTML used for display of checkbox.
+     */
+    public function admin_form_element( $id, $value )
+    {
+        // If the checkboxes value is one...
+        if( 1 == $value ) {
+            // ...this variable to checked.
+            $checked = 'checked';
+        } else {
+            // ...else leave the variable empty.
+            $checked = '';
+        }
+
+        // Return HTML to be output to the submission edit page.
+        return "<input type='hidden' name='fields[$id]' value='0' >
+                <input type='checkbox' name='fields[$id]' id='' $checked>";
     }
 }
