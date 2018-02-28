fc864e62cf88d80559a1c03c889e297aad815be4
Danny <dannyvankooten@gmail.com>
2018-02-28 16:08:33 +0100

    allow adding request body to exception class

--- includes/api/class-exception.php
+++ includes/api/class-exception.php
@@ -8,15 +8,14 @@ class MC4WP_API_Exception extends Exception {
     public $response;
 
     /**
-    * @var mixed
+    * @var object
     */
-    public $data;
-    public $type = '';
-    public $title = '';
-    public $status = '';
-    public $detail = '';
-    public $instance = '';
-    public $errors = array();
+    public $response_data = array();
+
+    /**
+    * @var object
+    */
+    public $request_data = array();
 
     /**
      * MC4WP_API_Exception constructor.
@@ -24,23 +23,13 @@ class MC4WP_API_Exception extends Exception {
      * @param string $message
      * @param int $code
      * @param array $response
-     * @param mixed $data
+     * @param object $data
      */
     public function __construct( $message, $code, $response = null, $data = null ) {
         parent::__construct( $message, $code );
 
         $this->response = $response;
-        $this->data = $data;
-
-        if( ! empty( $data ) ) {
-            // fill error properties from json data
-            $error_properties = array( 'type', 'title', 'status', 'detail', 'instance', 'errors' );
-            foreach( $error_properties as $key ) {
-                if( ! empty( $data->$key ) ) {
-                    $this->$key = $data->$key;
-                }
-            }
-        }
+        $this->response_data = $data;
     }
 
     /**
@@ -49,33 +38,40 @@ class MC4WP_API_Exception extends Exception {
     public function __toString() {
         $string = $this->message . '.';
 
-        // add title
-        if( ! empty( $this->title ) ) {
-            $string .= ' ' . $this->title . '.';
-        }
-
-        // add detail message
-        if( ! empty( $this->detail ) ) {
-            $string .= ' ' . $this->detail;
-        }
-
-        // add field specific errors
-        if( ! empty( $this->errors ) && isset( $this->errors[0]->field ) ) {
-
-            // strip off obsolete msg
-            $string = str_replace( 'For field-specific details, see the \'errors\' array.', '', $string );
-
-            // generate list of field errors
-            $field_errors = array();
-            foreach( $this->errors as $error ) {
-                if( ! empty( $error->field ) ) {
-                    $field_errors[] = sprintf( '- %s : %s', $error->field, $error->message );
-                } else {
-                    $field_errors[] = sprintf( '- %s', $error->message );
-                }
+        // add errors from response data returned by MailChimp
+        if( ! empty( $this->response_data ) ) {
+            if( ! empty( $this->response_data->title ) ) {
+                $string .= ' ' . $this->response_data->title . '.';
             }
 
-            $string .= " \n" . join( "\n", $field_errors );
+            // add detail message
+            if( ! empty( $this->response_data->detail ) ) {
+                $string .= ' ' . $this->response_data->detail;
+            }
+
+            // add field specific errors
+            if( ! empty( $this->response_data->errors ) && isset( $this->response_data->errors[0]->field ) ) {
+
+                // strip off obsolete msg
+                $string = str_replace( 'For field-specific details, see the \'errors\' array.', '', $string );
+
+                // generate list of field errors
+                $field_errors = array();
+                foreach( $this->response_data->errors as $error ) {
+                    if( ! empty( $error->field ) ) {
+                        $field_errors[] = sprintf( '- %s : %s', $error->field, $error->message );
+                    } else {
+                        $field_errors[] = sprintf( '- %s', $error->message );
+                    }
+                }
+
+                $string .= " \n" . join( "\n", $field_errors );
+            }
+        }
+
+        // Add request data to the string representation of this error, if set.
+        if( ! empty( $this->request_data ) ) {
+             $string .= "\n" . sprintf( 'Request body: %s', json_encode( $this->request_data ) );
         }
 
         return $string;
