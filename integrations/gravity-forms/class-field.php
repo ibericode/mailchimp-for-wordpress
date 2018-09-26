f6186bcef3658ef74d250bf12fa5d5a6395204b5
Danny van Kooten <dannyvankooten@gmail.com>
2018-09-26 10:50:35 +0200

    run mc4wp_integration_gravity-forms_options filter on gf choice config array for keys label & precheck

--- integrations/gravity-forms/class-field.php
+++ integrations/gravity-forms/class-field.php
@@ -63,18 +63,31 @@ class MC4WP_Gravity_Forms_Field extends GF_Field {
         return sprintf( "<div class='ginput_container ginput_container_checkbox'><ul class='gfield_checkbox' id='%s'>%s</ul></div>", esc_attr( $field_id ), $this->get_checkbox_choices( $value, $disabled_text, $form_id ) );
     }
 
+    private function apply_mc4wp_options_filters( $options ) {
+        $options = apply_filters( 'mc4wp_gravity-forms_integration_options', $options );
+        $options = apply_filters( 'mc4wp_integration_gravity-forms_options', $options );
+        return $options;
+    }
+
 
     public function get_checkbox_choices( $value, $disabled_text, $form_id = 0 ) {
         $choices = '';
         $is_entry_detail = $this->is_entry_detail();
         $is_form_editor  = $this->is_form_editor();
 
-        $choice = array(
-            'text' => $this->get_field_label( false, $value ),
-            'value' => "1",
-            'isSelected' => false,
+        $options = array(
+            'label' => $this->get_field_label( false, $value ),
+            'precheck' => false, 
         );
+        $options = $this->apply_mc4wp_options_filters( $options );
 
+        // generate html
+        $choice = array(
+            'text' => $options['label'],
+            'value' => "1",
+            'isSelected' => $options['precheck'],
+        );
+        
         $input_id = $this->id;
         if ( $is_entry_detail || $is_form_editor || $form_id == 0 ){
             $id = $this->id;
