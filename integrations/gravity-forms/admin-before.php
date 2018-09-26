1becf72075b3160ec053949bb73529d133ee4631
Danny van Kooten <dannyvankooten@gmail.com>
2018-09-26 10:17:29 +0200

    change name of field on gravity forms integration settings page

--- integrations/gravity-forms/admin-before.php
+++ integrations/gravity-forms/admin-before.php
@@ -1,3 +1,3 @@
 <p>
-    <?php echo sprintf( __( 'To integrate with Gravity Forms, add the "MailChimp" field to <a href="%s">one of your Gravity Forms forms</a>.', 'mailchimp-for-wp' ), admin_url( 'admin.php?page=gf_edit_forms' ) ); ?>
+    <?php echo sprintf( __( 'To integrate with Gravity Forms, add the "MailChimp for WordPress" field to <a href="%s">one of your Gravity Forms forms</a>.', 'mailchimp-for-wp' ), admin_url( 'admin.php?page=gf_edit_forms' ) ); ?>
 </p>
