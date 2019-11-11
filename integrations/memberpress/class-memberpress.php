b224bd06f07b543b11178f45f5f37243062e0a7f
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2019-11-11 10:31:16 +0100

    run phpcbf on latest merge

--- integrations/memberpress/class-memberpress.php
+++ integrations/memberpress/class-memberpress.php
@@ -7,73 +7,70 @@ defined( 'ABSPATH' ) or exit;
  *
  * @ignore
  */
-class MC4WP_MemberPress_Integration extends MC4WP_Integration
-{
-
-    /**
-     * @var string
-     */
-    public $name = "MemberPress";
-
-    /**
-     * @var string
-     */
-    public $description = "Subscribes people from MemberPress register forms.";
+class MC4WP_MemberPress_Integration extends MC4WP_Integration {
 
 
-    /**
-     * Add hooks
-     */
-    public function add_hooks()
-    {
-        if (! $this->options['implicit']) {
-            if (has_action('mepr_checkout_before_submit') {
-                add_action('mepr_checkout_before_submit', array( $this, 'output_checkbox' ));
-            } else {
-                add_action('mepr-checkout-before-submit', array( $this, 'output_checkbox' ));
-            }
-        }
+	/**
+	 * @var string
+	 */
+	public $name = 'MemberPress';
 
-        if (has_action('mepr_checkout_before_submit') {
-            add_action('mepr_signup', array( $this, 'subscribe_from_memberpress' ), 5);
-        } else {
-            add_action('mepr-signup', array( $this, 'subscribe_from_memberpress' ), 5);
-        }
-    }
+	/**
+	 * @var string
+	 */
+	public $description = 'Subscribes people from MemberPress register forms.';
 
 
+	/**
+	 * Add hooks
+	 */
+	public function add_hooks() {
+		if ( ! $this->options['implicit'] ) {
+			if ( has_action( 'mepr_checkout_before_submit' ) {
+				add_action( 'mepr_checkout_before_submit', array( $this, 'output_checkbox' ) );
+			} else {
+				add_action( 'mepr-checkout-before-submit', array( $this, 'output_checkbox' ) );
+			}
+		}
 
-    /**
-     * Subscribe from MemberPress sign-up forms.
-     *
-     * @param MeprTransaction $txn
-     * @return bool
-     */
-    public function subscribe_from_memberpress($txn)
-    {
+		if ( has_action( 'mepr_checkout_before_submit' ) {
+			add_action( 'mepr_signup', array( $this, 'subscribe_from_memberpress' ), 5 );
+		} else {
+			add_action( 'mepr-signup', array( $this, 'subscribe_from_memberpress' ), 5 );
+		}
+	}
 
-        // Is this integration triggered? (checkbox checked or implicit)
-        if (! $this->triggered()) {
-            return false;
-        }
 
-        $user = get_userdata($txn->user_id);
 
-        $data = array(
-            'EMAIL' => $user->user_email,
-            'FNAME' => $user->first_name,
-            'LNAME' => $user->last_name
-        );
+	/**
+	 * Subscribe from MemberPress sign-up forms.
+	 *
+	 * @param MeprTransaction $txn
+	 * @return bool
+	 */
+	public function subscribe_from_memberpress( $txn ) {
 
-        // subscribe using email and name
-        return $this->subscribe($data, $txn->id);
-    }
+		// Is this integration triggered? (checkbox checked or implicit)
+		if ( ! $this->triggered() ) {
+			return false;
+		}
 
-    /**
-     * @return bool
-     */
-    public function is_installed()
-    {
-        return defined('MEPR_VERSION');
-    }
+		$user = get_userdata( $txn->user_id );
+
+		$data = array(
+			'EMAIL' => $user->user_email,
+			'FNAME' => $user->first_name,
+			'LNAME' => $user->last_name,
+		);
+
+		// subscribe using email and name
+		return $this->subscribe( $data, $txn->id );
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function is_installed() {
+		return defined( 'MEPR_VERSION' );
+	}
 }
