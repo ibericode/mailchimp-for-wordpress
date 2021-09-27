30a10ee5489f9bfa4c27ae2203530a00a6d0e19a
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-09-27 11:38:05 +0200

    upgrade deprecated AMP function

--- includes/forms/class-form-amp.php
+++ includes/forms/class-form-amp.php
@@ -22,7 +22,7 @@ class MC4WP_Form_AMP {
 	 * @return string    Modified $content.
 	 */
 	public function add_response_templates( $content, $form ) {
-		if ( ! function_exists( 'is_amp_endpoint' ) || ! is_amp_endpoint() ) {
+		if ( ! function_exists( 'amp_is_request' ) || ! amp_is_request() ) {
 			return $content;
 		}
 
@@ -65,7 +65,7 @@ class MC4WP_Form_AMP {
 	 * @return array Modified $attributes.
 	 */
 	public function add_amp_request( $attributes ) {
-		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+		if ( function_exists( 'amp_is_request' ) && amp_is_request() ) {
 			$attributes['action-xhr'] = get_rest_url( null, 'mc4wp/v1/form' );
 		}
 
@@ -79,7 +79,7 @@ class MC4WP_Form_AMP {
 	 * @return bool Modified $load_scripts.
 	 */
 	public function suppress_scripts( $load_scripts ) {
-		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+		if ( function_exists( 'amp_is_request' ) && amp_is_request() ) {
 			return false;
 		}
 
