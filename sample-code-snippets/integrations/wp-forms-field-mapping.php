d012153468b5a9301f0540e3f7801047bf621fe4
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-06 10:10:42 +0100

    merge latest code snippets

---
+++ sample-code-snippets/integrations/wp-forms-field-mapping.php
@@ -0,0 +1,9 @@
+<?php
+// The problem with WPForms forms in this regard is that it generates field names that aren't always easy for our plugin to recognize. 
+// You will have to inspect the source of the form to find field names (name="...") for each field and manually map them as such:
+
+add_filter( 'mc4wp_integration_wpforms_subscriber_data', function( MC4WP_MailChimp_Subscriber $subscriber ) {
+	$subscriber->merge_fields[ "FNAME" ] = sanitize_text_field( $_POST['wpforms[fields][7][first]'] );
+	$subscriber->merge_fields[ "COUNTRY" ] = sanitize_text_field( $_POST['wpforms[fields][7][country]'] );
+	return $subscriber;
+});
---
+++ sample-code-snippets/premium/overwrite_domain_suggestions.php
@@ -0,0 +1,33 @@
+<?php
+/*
+With the form setting "Enable autocomplete for email domains?" is set to Yes, domain name automcompletions are suggested when typing an email address into the email field of the form. 
+
+With the filter 'mc4wp_forms_email_domain_suggestions' you can overwrite what domains are suggested. The expected return for the filter is an array of domain names. 
+
+Note that the order of the domains in the array of domains is the order in which they are suggested. 
+*/
+
+
+//To add 1 domain to the suggestions without overwriting the others
+add_filter(
+	'mc4wp_forms_email_domain_suggestions',
+	function( $domains ) {
+		$domains[] = 'thisdomainisaddedtothesuggestions.com';
+		return $domains;
+	}
+);
+
+
+//To repleace the suggestions with your own
+add_filter(
+	'mc4wp_forms_email_domain_suggestions',
+	function( $domains ) {
+		$domains = array(
+			"gmail.com",
+			"yahoo.com",
+			"hotmail.com",
+			"aol.com"
+      );
+		return $domains;
+	}
+);
