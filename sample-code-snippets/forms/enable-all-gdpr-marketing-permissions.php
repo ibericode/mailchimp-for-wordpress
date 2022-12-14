51e96f1b0e9d04c8a536fbbcc00b3961337195ca
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-14 14:34:31 +0100

    fix snippet for enabling all gdpr marketing permissions

--- sample-code-snippets/forms/enable-all-gdpr-marketing-permissions.php
+++ sample-code-snippets/forms/enable-all-gdpr-marketing-permissions.php
@@ -4,7 +4,8 @@
  * This snippet enables every marketing permission in the list a new subscriber is about to be added to (or updated in)
  */
 add_filter( 'mc4wp_subscriber_data', function( $subscriber, $list_id ) {
-	$marketing_permissions = $this->mailchimp->get_list_marketing_permissions( $list_id );
+    $mailchimp = new MC4WP_MailChimp();
+	$marketing_permissions = $mailchimp->get_list_marketing_permissions( $list_id );
 	foreach ( $marketing_permissions as $mp ) {
 		$subscriber->marketing_permissions[] = (object) array(
 			'marketing_permission_id' => $mp->marketing_permission_id,
