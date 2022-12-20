d13837bdd756f0c42194e53fa3bc250b0537e571
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2022-12-20 09:53:49 +0100

    remove quotes around form ID in shortcode example

--- includes/forms/views/edit-form.php
+++ includes/forms/views/edit-form.php
@@ -64,7 +64,7 @@ $tabs = apply_filters( 'mc4wp_admin_edit_form_tabs', $tabs );
 						   style="line-height: initial;">
 				</div>
 				<div>
-					<?php echo sprintf( esc_html__( 'Use the shortcode %s to display this form inside a post, page or text widget.', 'mailchimp-for-wp' ), '<input type="text" onfocus="this.select();" readonly="readonly" value="' . esc_attr( sprintf( '[mc4wp_form id="%d"]', $form->ID ) ) . '" size="' . ( strlen( $form->ID ) + 18 ) . '">' ); ?>
+					<?php echo sprintf( esc_html__( 'Use the shortcode %s to display this form inside a post, page or text widget.', 'mailchimp-for-wp' ), '<input type="text" onfocus="this.select();" readonly="readonly" value="' . esc_attr( sprintf( '[mc4wp_form id=%d]', $form->ID ) ) . '" size="' . ( strlen( $form->ID ) + 15 ) . '">' ); ?>
 				</div>
 			</div>
 
