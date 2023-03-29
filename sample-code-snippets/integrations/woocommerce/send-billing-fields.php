7f9cfd8ace5e2a69390410ca6e221fa23b2d1f95
Arne Lap <arnelap@users.noreply.github.com>
2023-03-29 20:26:52 +0700

    Update send-billing-fields.php

--- sample-code-snippets/integrations/woocommerce/send-billing-fields.php
+++ sample-code-snippets/integrations/woocommerce/send-billing-fields.php
@@ -12,6 +12,10 @@ add_filter( 'mc4wp_integration_woocommerce_data', function( $data, $order_id ) {
 
 	// if it's a custom checkout field, usually you can get its value like this:
 	$data[ 'NAME_OF_FIELD_IN_MAILCHIMP' ] = $order->get_meta( 'name_of_field_in_woocommerce', true );
+	
+	// this sends the billing_phone field from WooCommerce to a Mailchimp field called "PHONE". 
+	// I recommend using a text field, so that any format of phone number will pass validation in Mailchimp.
+	$data[ 'PHONE' ] = $order->get_billing_phone();
 
 	return $data;
 }, 10, 2);
