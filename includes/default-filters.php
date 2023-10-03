9bfc7fabf8bfb0b1c598e25580cfc006406274fb
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2023-10-03 18:56:21 +0200

    get rid of unnecessary substraction for maximum filter priority

--- includes/default-filters.php
+++ includes/default-filters.php
@@ -5,10 +5,10 @@ defined( 'ABSPATH' ) or exit;
 add_filter( 'mc4wp_form_data', 'mc4wp_add_name_data', 60 );
 add_filter( 'mc4wp_integration_data', 'mc4wp_add_name_data', 60 );
 
-add_filter( 'mctb_data', '_mc4wp_update_groupings_data', PHP_INT_MAX - 1 );
-add_filter( 'mc4wp_form_data', '_mc4wp_update_groupings_data', PHP_INT_MAX - 1 );
-add_filter( 'mc4wp_integration_data', '_mc4wp_update_groupings_data', PHP_INT_MAX - 1 );
-add_filter( 'mailchimp_sync_user_data', '_mc4wp_update_groupings_data', PHP_INT_MAX - 1 );
+add_filter( 'mctb_data', '_mc4wp_update_groupings_data', PHP_INT_MAX );
+add_filter( 'mc4wp_form_data', '_mc4wp_update_groupings_data', PHP_INT_MAX );
+add_filter( 'mc4wp_integration_data', '_mc4wp_update_groupings_data', PHP_INT_MAX );
+add_filter( 'mailchimp_sync_user_data', '_mc4wp_update_groupings_data', PHP_INT_MAX );
 add_filter( 'mc4wp_use_sslverify', '_mc4wp_use_sslverify', 1 );
 
 mc4wp_apply_deprecated_filters( 'mc4wp_merge_vars', 'mc4wp_form_data' );
