4a3a79d48cacf696be2fec3c42a2dcd92a924f26
Jaime Martínez <info@jaimemartinez.nl>
2024-08-06 01:36:59 +0200

    Change docs filter `mc4wp_gravity-forms_integration_options` to `mc4wp_integration_gravity-forms_options`

--- sample-code-snippets/integrations/filter-integration-options.php
+++ sample-code-snippets/integrations/filter-integration-options.php
@@ -3,7 +3,7 @@
 /**
 * This filter enables the "update existing subscribers" option for the MailChimp for WordPress Gravity Forms integration
 */
-add_filter( 'mc4wp_gravity-forms_integration_options', function( $opts ) {
+add_filter( 'mc4wp_integration_gravity-forms_options', function( $opts ) {
 	$opts['update_existing'] = true;
 	return $opts;
 });
