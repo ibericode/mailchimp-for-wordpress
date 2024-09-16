3df4be9f393a7ad95ad4ac8fc4ac2b485d758588
Danny van Kooten <hi@dvk.co>
2024-09-16 16:09:25 +0200

    escape attributes in default form content using esc_attr instead of esc_html

--- config/default-form-content.php
+++ config/default-form-content.php
@@ -1,11 +1,11 @@
 <?php
 
 $email_label       = esc_html__('Email address', 'mailchimp-for-wp');
-$email_placeholder = esc_html__('Your email address', 'mailchimp-for-wp');
-$signup_button     = esc_html__('Sign up', 'mailchimp-for-wp');
+$email_placeholder_attr = esc_attr__('Your email address', 'mailchimp-for-wp');
+$signup_button_value     = esc_attr__('Sign up', 'mailchimp-for-wp');
 
 $content  = "<p>\n\t<label>{$email_label}: \n";
-$content .= "\t\t<input type=\"email\" name=\"EMAIL\" placeholder=\"{$email_placeholder}\" required />\n</label>\n</p>\n\n";
-$content .= "<p>\n\t<input type=\"submit\" value=\"{$signup_button}\" />\n</p>";
+$content .= "\t\t<input type=\"email\" name=\"EMAIL\" placeholder=\"{$email_placeholder_attr}\" required />\n</label>\n</p>\n\n";
+$content .= "<p>\n\t<input type=\"submit\" value=\"{$signup_button_value}\" />\n</p>";
 
 return $content;
