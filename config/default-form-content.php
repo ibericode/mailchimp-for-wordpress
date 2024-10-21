2b8bbd3a6eab5a7f07e1403497a2700283459e3a
Danny van Kooten <hi@dvk.co>
2024-10-21 09:59:32 +0200

    update default form content to include 'for' attribute on label field

--- config/default-form-content.php
+++ config/default-form-content.php
@@ -4,8 +4,8 @@ $email_label       = esc_html__('Email address', 'mailchimp-for-wp');
 $email_placeholder_attr = esc_attr__('Your email address', 'mailchimp-for-wp');
 $signup_button_value     = esc_attr__('Sign up', 'mailchimp-for-wp');
 
-$content  = "<p>\n\t<label>{$email_label}: \n";
-$content .= "\t\t<input type=\"email\" name=\"EMAIL\" placeholder=\"{$email_placeholder_attr}\" required />\n</label>\n</p>\n\n";
-$content .= "<p>\n\t<input type=\"submit\" value=\"{$signup_button_value}\" />\n</p>";
+$content  = "<p>\n\t<label for=\"email\">{$email_label}: \n";
+$content .= "\t\t<input type=\"email\" id=\"email\" name=\"EMAIL\" placeholder=\"{$email_placeholder_attr}\" required>\n</label>\n</p>\n\n";
+$content .= "<p>\n\t<input type=\"submit\" value=\"{$signup_button_value}\">\n</p>";
 
 return $content;
