33bc11cd56fead0925a375b39633f9728e950e32
Arne Lap <arnelap@users.noreply.github.com>
2025-12-05 16:07:27 +0100

    small comment change

--- sample-code-snippets/integrations/contact-form-7/forwarding-cf7-checkboxes-to-mailchimp.php
+++ sample-code-snippets/integrations/contact-form-7/forwarding-cf7-checkboxes-to-mailchimp.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * This code snippet will check if any of the CF7 forms is an array (checkboxes) and convert it to a comma separated string
+ * This code snippet will check if any of the CF7 fields is an array (checkbox) and convert it to a comma separated string.
  * This way it can be saved in a normal text field in your Mailchimp audience.
  *
- * In your CF7 form make sure the Checkbox name has the mc4wp- prefix.
- * For example if you want to save the values of the checkbox in text field called MMERGE9 in Mailchimp, name the checkbox mc4wp-MMERGE9
+ * In your CF7 form make sure the Checkbox name has the mc4wp- prefix, eg. mc4wp-MMERGE9
  */
 
 add_filter('mc4wp_integration_contact-form-7_subscriber_data', function (MC4WP_MailChimp_Subscriber $subscriber, $cf7_form_id) {
