bb41bbf2cdf0509d9d9005e6ae80a35b96e10edf
Arne Lap <arnelap@users.noreply.github.com>
2025-12-05 15:16:00 +0100

    add empty array fallback to prevent warning in php log

--- sample-code-snippets/integrations/contact-form-7/forwarding-cf7-checkboxes-to-mailchimp.php
+++ sample-code-snippets/integrations/contact-form-7/forwarding-cf7-checkboxes-to-mailchimp.php
@@ -8,10 +8,10 @@
  * This code takes the checkbox named CHECKBOX1 from the CF7 form, and translates it to a semicolon separated string and send that to the Mailchimp field MMERGE8
  * 
  * Change MMERGE8 to your Mailchimp field and CHECKBOX1 to your CF7 field. 
+ * The checkbox name in CF7 still needs the mc4wp- prefix, eg mc4wp-checkbox-1
  */
 
 add_filter( 'mc4wp_integration_contact-form-7_data', function( $data ) {
-   $data['MMERGE8'] = join( ';', $data['CHECKBOX1'] );
-   unset($data['CHECKBOX1']); 
+   $data['MMERGE8'] = join( ';', $data['CHECKBOX-1'] ?? [] );
    return $data;
 });
