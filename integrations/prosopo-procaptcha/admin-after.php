7469bc63603873a247b05388135eb3f42e1e6c66
Arne Lap <arnelap@users.noreply.github.com>
2025-12-10 13:29:54 +0100

    updated Proposo "get api key" link to go directly to API screen instead of user profile

--- integrations/prosopo-procaptcha/admin-after.php
+++ integrations/prosopo-procaptcha/admin-after.php
@@ -54,7 +54,7 @@ echo $procaptcha_api->print_captcha_element(true, true);
             sprintf(
                 // translators: %1$s: opening anchor tag, %2$s: closing anchor tag
                 esc_html__('The API key for connecting with your Procaptcha account. %1$s Get your Site key here %2$s', 'mailchimp-for-wp'),
-                '<a href="https://portal.prosopo.io/" target="_blank">',
+                '<a href="https://portal.prosopo.io/site-management" target="_blank">',
                 '</a>'
             );
             ?>
