42ff9d27b215208e453436c43fcbdca3a69708db
Danny van Kooten <hi@dvk.co>
2025-12-11 13:06:53 +0100

    fix cs

--- sample-code-snippets/premium/ecommerce/wpml-product-permalinks.php
+++ sample-code-snippets/premium/ecommerce/wpml-product-permalinks.php
@@ -9,20 +9,20 @@
  */
 
 add_filter('mc4wp_ecommerce_product_data', function ($data, WC_Product $product) {
-    $language = apply_filters('wpml_element_language_code', null, array(
+    $language = apply_filters('wpml_element_language_code', null, [
         'element_id' => $data['id'],
         'element_type' => 'product'
-    ));
+    ]);
     $data['url'] = apply_filters('wpml_permalink', $data['url'], $language);
     return $data;
 }, 10, 2);
 
 add_filter('mc4wp_ecommerce_product_variants_data', function ($variants, $product) {
     foreach ($variants as $key => $variant) {
-        $language = apply_filters('wpml_element_language_code', null, array(
+        $language = apply_filters('wpml_element_language_code', null, [
             'element_id' => $variant['id'],
             'element_type' => 'product'
-        ));
+        ]);
         $variants[$key]['url'] = apply_filters('wpml_permalink', $variant['url'], $language);
     }
     return $variants;
