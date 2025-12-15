eaa2627c4f524c576496af4b41a399105efd90b5
Arne Lap <arnelap@users.noreply.github.com>
2025-12-15 17:10:27 +0100

    Updated code snippets to showcase remove tags

--- sample-code-snippets/integrations/contact-form-7/different-tag.php
+++ sample-code-snippets/integrations/contact-form-7/different-tag.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Add a different tag to the subscriber based on the CF7 form ID.
+ * In this example CF7 form with ID 500 will get the tag ES.
+ * The form ID 510 will get the tag NL
+ *
+ * It is also possible to remove tags, see the subscriber-tags.php snippet under /forms/
+ */
+
 add_filter('mc4wp_integration_contact-form-7_subscriber_data', function (MC4WP_MailChimp_Subscriber $subscriber, $cf7_form_id) {
     if ($cf7_form_id == 500) {
         $subscriber->tags[] = 'ES';
--- sample-code-snippets/integrations/ninja-forms/different-tag-per-form.php
+++ sample-code-snippets/integrations/ninja-forms/different-tag-per-form.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Add a different tag to the subscriber based on the Ninja Forms form ID.
+ * In this example Ninja Forms form with ID 500 will get the tag ES.
+ * The form ID 510 will get the tag NL
+ *
+ * It is also possible to remove tags, see the subscriber-tags.php snippet under /forms/
+ */
+
 add_filter('mc4wp_integration_ninja-forms_subscriber_data', function (MC4WP_MailChimp_Subscriber $subscriber, $form_id) {
     if ($form_id == 500) {
         $subscriber->tags[] = 'ES';
